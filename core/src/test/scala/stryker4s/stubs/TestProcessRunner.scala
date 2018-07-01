312d75bd201aa32838bcb1d29e15b6440927e95f
Legopiraat <gijs.venne@hotmail.com>
2018-07-01 20:38:23 +0200

    Add explination on how the TestProcessRunner returns its values

--- core/src/test/scala/stryker4s/stubs/TestProcessRunner.scala
+++ core/src/test/scala/stryker4s/stubs/TestProcessRunner.scala
@@ -5,11 +5,18 @@ import stryker4s.run.process.ProcessRunner
 
 import scala.util.Try
 
-class TestProcessRunner(returns: Try[Int]*) extends ProcessRunner {
+class TestProcessRunner(testRunExitCode: Try[Int]*) extends ProcessRunner {
   val timesCalled: Iterator[Int] = Iterator.from(0)
 
+  /**
+    * Keep track on the amount of times the function is called.
+    * Also return an exit code which the test runner would do as well.
+    */
   override def apply(command: String, workingDir: File, envVar: (String, String)): Try[Int] = {
     timesCalled.next()
-    returns(envVar._2.toInt - 1)
+
+    //Get the currently active test run and subtract 1 because currently the test runs start at 1 and not 0.
+    val testRun = envVar._2.toInt - 1
+    testRunExitCode(testRun)
   }
 }
