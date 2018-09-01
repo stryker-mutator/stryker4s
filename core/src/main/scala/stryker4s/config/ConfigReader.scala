a5356dfdc58d77ecc8f60caad888ad7d75cf74f8
Philipp Weissenbacher <philipp.weissenbacher@gmail.com>
2018-09-01 10:53:22 +0200

    Allow overriding the log level from the config file (#49)
    * Allow overriding the log level from the config file
    
    You now can set the log level from the stryker4s.conf file using the
    `log-level` property
    
    Accepted values are ERROR, WARN, INFO, DEBUG, TRACE, ALL, OFF

--- core/src/main/scala/stryker4s/config/ConfigReader.scala
+++ core/src/main/scala/stryker4s/config/ConfigReader.scala
@@ -4,6 +4,7 @@ import java.io.FileNotFoundException
 import java.nio.file.Path
 
 import better.files.File
+import ch.qos.logback.classic.Level
 import grizzled.slf4j.Logging
 import pureconfig.error.{CannotReadFile, ConfigReaderException, ConfigReaderFailures}
 import pureconfig.{ConfigReader => PConfigReader}
@@ -13,6 +14,8 @@ object ConfigReader extends Logging {
     *
     */
   private implicit val toFileReader: PConfigReader[File] = PConfigReader[Path].map(p => File(p))
+  private implicit val logLevelReader: PConfigReader[Level] = PConfigReader[String] map (level =>
+    Level.toLevel(level))
 
   /** Read config from stryker4s.conf. Or use the default Config if no config file is found.
     */
@@ -20,7 +23,7 @@ object ConfigReader extends Logging {
     pureconfig.loadConfig[Config](confFile.path, namespace = "stryker4s") match {
       case Left(failures) => tryRecoverFromFailures(failures)
       case Right(config) =>
-        info("Using stryker4s.conf in the current working directory")
+        debug("Using stryker4s.conf in the current working directory")
         config
     }
 
@@ -28,8 +31,11 @@ object ConfigReader extends Logging {
     case ConfigReaderFailures(CannotReadFile(fileName, Some(_: FileNotFoundException)), _) =>
       warn(s"Could not find config file $fileName")
       warn("Using default config instead...")
+
       val defaultConf = Config()
+
       debug("Config used: " + defaultConf.toHoconString)
+
       defaultConf
     case _ =>
       error("Failures in reading config: ")
