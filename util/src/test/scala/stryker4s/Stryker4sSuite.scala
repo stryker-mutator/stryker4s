0fe4087ce1aea9f2b9e8b7b99b579e512565e19d
Hugo van Rijswijk <hugo.v.rijswijk@gmail.com>
2018-09-21 13:52:46 +0200

    Refactor test code for a more generic way of Log message testing (#63)
    * Refactor all logging stuff into LogMatcher
    * Implement LogMatcher changes in MutantFinderTest, add BeforeAndAfterEach to Stryker4sSuite and tighten encapsulation in LogMatchers

--- util/src/test/scala/stryker4s/Stryker4sSuite.scala
+++ util/src/test/scala/stryker4s/Stryker4sSuite.scala
@@ -1,15 +1,10 @@
 package stryker4s
 
-import org.scalatest.{FunSpec, LoneElement, Matchers, OptionValues}
-import stryker4s.scalatest.LogMatchers
+import org.scalatest._
 
-trait Stryker4sSuite extends FunSpec with Matchers with OptionValues with LoneElement with LogMatchers {
-
-  /**
-    * The className of the system under test.
-    * Is done by getting the executing test class and stripping off 'test'.
-    *
-    * This is used for logMatchers.
-    */
-  implicit val className: String =  getClass.getCanonicalName.replace("Test", "")
-}
+trait Stryker4sSuite
+    extends FunSpec
+    with Matchers
+    with OptionValues
+    with LoneElement
+    with BeforeAndAfterEach
