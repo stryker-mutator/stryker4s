5bea444bff34375e1a856aa560aafa5cdebe1b41
Legopiraat <gijs.venne@hotmail.com>
2018-10-04 23:43:35 +0200

    Add tests for file collector

--- core/src/test/scala/stryker4s/mutants/findmutants/FileCollectorTest.scala
+++ core/src/test/scala/stryker4s/mutants/findmutants/FileCollectorTest.scala
@@ -1,12 +1,24 @@
 package stryker4s.mutants.findmutants
 
+import better.files.File
+import org.mockito.MockitoSugar
+import org.mockito.ArgumentMatchers._
 import stryker4s.Stryker4sSuite
 import stryker4s.config.Config
-import stryker4s.scalatest.FileUtil
+import stryker4s.run.process.{Command, ProcessRunner}
+import stryker4s.scalatest.{FileUtil, LogMatchers}
 
-class FileCollectorTest extends Stryker4sSuite {
-  describe("collectFiles") {
+import scala.util.{Failure, Try}
 
+class FileCollectorTest extends Stryker4sSuite with MockitoSugar with LogMatchers {
+
+  private val filledDirPath: File = FileUtil.getResource("fileTests/filledDir")
+  private val basePath: File = filledDirPath / "src/main/scala/package"
+
+  assume(filledDirPath.exists(), "Filled test dir does not exist")
+  assume(basePath.exists(), "Basepath dir does not exist")
+
+  describe("collect files to mutate") {
     describe("on empty dir") {
       val emptyDir = FileUtil.getResource("fileTests/emptyDir")
       assume(emptyDir.exists(), "Empty test dir does not exist")
@@ -22,11 +34,6 @@ class FileCollectorTest extends Stryker4sSuite {
     }
 
     describe("on filled dir") {
-      val filledDirPath = FileUtil.getResource("fileTests/filledDir")
-      val basePath = filledDirPath / "src/main/scala/package"
-
-      assume(filledDirPath.exists(), "Filled test dir does not exist")
-      assume(basePath.exists(), "Basepath dir does not exist")
 
       it("should find all scala files and not the non-scala files with default config") {
         implicit val config: Config = Config(baseDir = filledDirPath)
@@ -159,6 +166,78 @@ class FileCollectorTest extends Stryker4sSuite {
 
         results should have size 2
         results should contain only (basePath / "someFile.scala", basePath / "secondFile.scala")
+      }
+    }
+  }
+
+  describe("Collect files to copy over to tmp folder") {
+
+    val processRunnerMock: ProcessRunner = mock[ProcessRunner]
+
+    it("Should execute git process to collect files"){
+      implicit val config: Config = Config(baseDir = filledDirPath)
+      val filePath = "Config.scala"
+      val expectedFileList = Seq(config.baseDir / filePath)
+      val gitProcessResult = Try(Seq(filePath))
+      when(processRunnerMock(Command("git ls-files", "--others --exclude-standard --cached"), config.baseDir)).thenReturn(gitProcessResult)
+
+      val sut = new FileCollector()
+
+      val results = sut.filesToCopy(processRunnerMock)
+
+      results should contain theSameElementsAs expectedFileList
+    }
+
+    it("Should copy the files from the files config key when the target repo is not a git repo") {
+      implicit val config: Config = Config(baseDir = filledDirPath, files = Some(Seq("**/main/scala/**/*.scala")))
+      val expectedFileList = Seq(basePath / "someFile.scala", basePath / "secondFile.scala")
+      val gitProcessResult = Failure(new Exception("Exception"))
+      when(processRunnerMock(any[Command], any[File])).thenReturn(gitProcessResult)
+
+      val sut = new FileCollector()
+
+      val results = sut.filesToCopy(processRunnerMock)
+
+      results should contain theSameElementsAs expectedFileList
+    }
+
+    it("Should copy all files when no files config key is found and target repo is not a git repo") {
+      implicit val config: Config = Config(baseDir = filledDirPath, files = None)
+      val expectedFileList = Seq(basePath / "someFile.scala", basePath / "secondFile.scala")
+      val gitProcessResult = Failure(new Exception("Exception"))
+      when(processRunnerMock(any[Command], any[File])).thenReturn(gitProcessResult)
+
+      val sut = new FileCollector()
+
+      val results = sut.filesToCopy(processRunnerMock)
+
+      //Should be zero because target is in the name of the path.
+      results should have size 0
+    }
+
+    describe("log tests"){
+      it("Should log that no git repo is found and is using fallback") {
+        implicit val config: Config = Config(baseDir = filledDirPath)
+        val gitProcessResult = Failure(new Exception(""))
+        when(processRunnerMock(any[Command], any[File])).thenReturn(gitProcessResult)
+
+        val sut = new FileCollector()
+
+        sut.filesToCopy(processRunnerMock)
+
+        "Not a git repo falling back to 'files' configuration." shouldBe loggedAsInfo
+      }
+
+      it("Should log that no files config option is found and is using fallback to copy all files") {
+        implicit val config: Config = Config(baseDir = filledDirPath)
+        val gitProcessResult = Failure(new Exception(""))
+        when(processRunnerMock(any[Command], any[File])).thenReturn(gitProcessResult)
+
+        val sut = new FileCollector()
+
+        sut.filesToCopy(processRunnerMock)
+
+        "No 'files' specified falling back to copying everything excluding target" shouldBe loggedAsWarning
       }
     }
   }
