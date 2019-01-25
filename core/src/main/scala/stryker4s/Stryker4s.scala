7dbe0b22934e0d02bc980b0d4524db51cbd3fb64
Wouter Aarts <Wouter.Aarts@infosupport.com>
2019-01-25 14:48:10 +0100

    Log warning when the amount of memory the JVM has seems low.

--- core/src/main/scala/stryker4s/Stryker4s.scala
+++ core/src/main/scala/stryker4s/Stryker4s.scala
@@ -1,5 +1,6 @@
 package stryker4s
 
+import grizzled.slf4j.Logging
 import stryker4s.config.Config
 import stryker4s.mutants.Mutator
 import stryker4s.mutants.findmutants.SourceCollector
@@ -8,13 +9,22 @@ import stryker4s.run.report.Reporter
 import stryker4s.run.threshold.{ScoreStatus, ThresholdChecker}
 
 class Stryker4s(fileCollector: SourceCollector, mutator: Mutator, runner: MutantRunner, reporter: Reporter)(
-    implicit config: Config) {
+    implicit config: Config) extends Logging {
 
   def run(): ScoreStatus = {
+    preRunLogs()
     val filesToMutate = fileCollector.collectFilesToMutate()
     val mutatedFiles = mutator.mutate(filesToMutate)
     val runResults = runner(mutatedFiles, fileCollector)
     reporter.report(runResults)
     ThresholdChecker.determineScoreStatus(runResults.mutationScore)
+  }
+
+  private def preRunLogs(): Unit = {
+    // If JVM max memory is less than 2000MB, log warning
+    if ((Runtime.getRuntime.maxMemory / 1024 / 1024) < 2000) {
+      warn("SBT doesn't have a lot of memory assigned to it. It's wise to increase the maximum memory SBT can use while running stryker.")
+      warn("This can be done by adding setting an environment variable: SBT_OPTS=\"-XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=4G -Xmx4G\" ")
+    }
   }
 }
