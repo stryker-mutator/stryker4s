0dcfe2436b261cb6f16cfd69d661eaa4a363ad5b
Hugo van Rijswijk <hugo.v.rijswijk@gmail.com>
2019-01-28 17:10:58 +0100

    Add maven invoker to call test goals

--- runners/maven/src/main/scala/stryker4s/run/MavenMutantRunner.scala
+++ runners/maven/src/main/scala/stryker4s/run/MavenMutantRunner.scala
@@ -2,13 +2,47 @@ package stryker4s.run
 import java.nio.file.Path
 
 import better.files.File
+import org.apache.maven.shared.invoker.{DefaultInvocationRequest, InvocationRequest, Invoker}
 import stryker4s.config.Config
-import stryker4s.model.{Mutant, MutantRunResult}
+import stryker4s.model.{Killed, Mutant, MutantRunResult, Survived}
+import stryker4s.mutants.findmutants.SourceCollector
 import stryker4s.run.process.ProcessRunner
 
-class MavenMutantRunner(processRunner: ProcessRunner)(implicit config: Config) extends MutantRunner(processRunner) {
+import scala.collection.JavaConverters._
 
-  override def runInitialTest(workingDir: File): Boolean = ???
+class MavenMutantRunner(invoker: Invoker, processRunner: ProcessRunner, sourceCollector: SourceCollector)(
+    implicit config: Config)
+    extends MutantRunner(processRunner, sourceCollector) {
 
-  override def runMutant(mutant: Mutant, workingDir: File, subPath: Path): MutantRunResult = ???
+  private val goal = List("test").asJava
+
+  override def runInitialTest(workingDir: File): Boolean = {
+    val request = createRequest(workingDir)
+
+    val result = invoker.execute(request)
+
+    result.getExitCode == 0
+  }
+
+  override def runMutant(mutant: Mutant, workingDir: File, subPath: Path): MutantRunResult = {
+    val request = createRequest(workingDir)
+
+    val result = invoker.execute(request)
+
+    result.getExitCode match {
+      case 0 => Survived(mutant, subPath)
+      case _ => Killed(mutant, subPath)
+    }
+  }
+
+  private def createRequest(sourceDir: File): InvocationRequest = {
+    val request = new DefaultInvocationRequest
+
+    // TODO: Set working dir
+    request.setGoals(goal)
+    request.setOutputHandler(debug(_))
+    request.setBatchMode(true)
+
+    request
+  }
 }
--- runners/maven/src/main/scala/stryker4s/run/Stryker4sMavenRunner.scala
+++ runners/maven/src/main/scala/stryker4s/run/Stryker4sMavenRunner.scala
@@ -1,8 +1,13 @@
 package stryker4s.run
+import org.apache.maven.shared.invoker.{DefaultInvoker, Invoker}
 import stryker4s.config.Config
+import stryker4s.mutants.findmutants.SourceCollector
 import stryker4s.run.process.ProcessRunner
 
 class Stryker4sMavenRunner extends Stryker4sRunner {
-  override def resolveRunner()(implicit config: Config): MutantRunner =
-    new MavenMutantRunner(ProcessRunner.resolveRunner())
+  override def resolveRunner(collector: SourceCollector)(implicit config: Config): MutantRunner =
+    new MavenMutantRunner(resolveInvoker, ProcessRunner.resolveRunner(), collector)
+
+  private def resolveInvoker: Invoker = new DefaultInvoker
+
 }
