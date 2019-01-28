dba4285c77d6e46960371319359974c37f302068
Hugo van Rijswijk <hugo.v.rijswijk@gmail.com>
2019-01-28 17:11:11 +0100

    Rename goal to run

--- runners/maven/src/main/scala/stryker4s/run/Stryker4sMavenPlugin.scala
+++ runners/maven/src/main/scala/stryker4s/run/Stryker4sMavenPlugin.scala
@@ -3,7 +3,7 @@ package stryker4s.run
 import org.apache.maven.plugin.AbstractMojo
 import org.apache.maven.plugins.annotations.Mojo
 
-@Mojo(name = "stryker")
+@Mojo(name = "run")
 class Stryker4sMavenPlugin extends AbstractMojo {
   override def execute(): Unit = {
     new Stryker4sMavenRunner().run()
