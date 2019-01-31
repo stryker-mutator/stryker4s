8808af2cdee89a9c53801faef93cc5b829eb1cea
Hugo van Rijswijk <hugo.v.rijswijk@gmail.com>
2019-01-31 13:15:03 +0100

    Add constructor injection to plugin

--- runners/maven/src/main/scala/stryker4s/run/MavenMutantRunner.scala
+++ runners/maven/src/main/scala/stryker4s/run/MavenMutantRunner.scala
@@ -3,26 +3,32 @@ import java.nio.file.Path
 import java.util.Properties
 
 import better.files._
-import org.apache.maven.model.Build
+import org.apache.maven.project.MavenProject
 import org.apache.maven.shared.invoker.{DefaultInvocationRequest, InvocationRequest, Invoker}
 import stryker4s.config.Config
+import stryker4s.extension.FileExtensions._
 import stryker4s.model.{Killed, Mutant, MutantRunResult, Survived}
 import stryker4s.mutants.findmutants.SourceCollector
 import stryker4s.run.process.ProcessRunner
 
 import scala.collection.JavaConverters._
 
-class MavenMutantRunner(build: Build, invoker: Invoker, processRunner: ProcessRunner, sourceCollector: SourceCollector)(
+class MavenMutantRunner(project: MavenProject,
+                        invoker: Invoker,
+                        processRunner: ProcessRunner,
+                        sourceCollector: SourceCollector)(
     implicit config: Config
 ) extends MutantRunner(processRunner, sourceCollector) {
 
   private val goals = List("test").asJava
-  private val properties = new Properties()
 
   override def runInitialTest(workingDir: File): Boolean = {
     // Set source once, settings is persistent among goals
-    build.setDirectory(workingDir.pathAsString) // TODO: Doesn't work yet
-
+    val newSources = project.getCompileSourceRoots.asScala
+      .map(sources => workingDir / File(sources).relativePath.toString) map (_.pathAsString)
+    debug("New sources: " + newSources.mkString(", "))
+    debug("Existing sources: " + project.getCompileSourceRoots)
+    newSources foreach project.addCompileSourceRoot
     val request = createRequest(workingDir)
     request.setGoals(goals)
 
@@ -32,9 +38,9 @@ class MavenMutantRunner(build: Build, invoker: Invoker, processRunner: ProcessRu
   }
 
   override def runMutant(mutant: Mutant, workingDir: File, subPath: Path): MutantRunResult = {
+    debug("Existing sources: " + project.getCompileSourceRoots)
     val request = createRequest(workingDir)
-    properties.setProperty("ACTIVE_MUTATION", String.valueOf(mutant.id))
-    request.setProperties(properties)
+    request.addShellEnvironment("ACTIVE_MUTATION", String.valueOf(mutant.id))
 
     val result = invoker.execute(request)
 
--- runners/maven/src/main/scala/stryker4s/run/Stryker4sMavenPlugin.scala
+++ runners/maven/src/main/scala/stryker4s/run/Stryker4sMavenPlugin.scala
@@ -1,15 +1,16 @@
 package stryker4s.run
 
-import org.apache.maven.model.Build
+import javax.inject.Inject
 import org.apache.maven.plugin.AbstractMojo
 import org.apache.maven.plugins.annotations.{Mojo, Parameter}
+import org.apache.maven.project.MavenProject
 
 /** The main goal for this plugin. Starts Stryker4s.
   */
 @Mojo(name = "run")
-class Stryker4sMavenPlugin(@Parameter(defaultValue = "${build}") build: Build) extends AbstractMojo {
-
+class Stryker4sMavenPlugin @Inject()(@Parameter(defaultValue = "${project}") project: MavenProject)
+    extends AbstractMojo {
   override def execute(): Unit = {
-    new Stryker4sMavenRunner(build).run()
+    new Stryker4sMavenRunner(project).run()
   }
 }
