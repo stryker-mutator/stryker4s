af08c259be9270affd4afbf693eb2a1a1b56ffef
Hugo van Rijswijk <hugo.v.rijswijk@gmail.com>
2019-02-07 13:00:04 +0100

    Fix implemented class

--- runners/maven/src/main/scala/stryker4s/run/Stryker4sMavenRunner.scala
+++ runners/maven/src/main/scala/stryker4s/run/Stryker4sMavenRunner.scala
@@ -2,12 +2,16 @@ package stryker4s.run
 import org.apache.maven.project.MavenProject
 import org.apache.maven.shared.invoker.{DefaultInvoker, Invoker}
 import stryker4s.config.Config
+import stryker4s.mutants.applymutants.ActiveMutationContext.envVar
+import stryker4s.mutants.applymutants.ActiveMutationContext.ActiveMutationContext
 import stryker4s.mutants.findmutants.SourceCollector
 import stryker4s.run.process.ProcessRunner
 
 class Stryker4sMavenRunner(project: MavenProject) extends Stryker4sRunner {
   override def resolveRunner(collector: SourceCollector)(implicit config: Config): MutantRunner =
     new MavenMutantRunner(project, resolveInvoker, ProcessRunner.resolveRunner(), collector)
+
+  override def mutationActivation: ActiveMutationContext = envVar
 
   private def resolveInvoker: Invoker = new DefaultInvoker
 }
