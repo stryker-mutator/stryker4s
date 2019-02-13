ea28843429548c661ab255120e042c475b6d0927
Hugo van Rijswijk <hugo.v.rijswijk@gmail.com>
2019-02-13 10:15:51 +0100

    Add test for Survived, Killed mutants and environment setup in Maven

--- runners/maven/src/test/scala/stryker4s/run/MavenMutantRunnerTest.scala
+++ runners/maven/src/test/scala/stryker4s/run/MavenMutantRunnerTest.scala
@@ -13,11 +13,12 @@ import org.mockito.captor.ArgCaptor
 import org.mockito.{ArgumentMatchersSugar, IdiomaticMockito, MockitoSugar}
 import stryker4s.extension.mutationtype.Or
 import stryker4s.model
-import stryker4s.model.{Killed, Mutant}
+import stryker4s.model.{Killed, Mutant, Survived}
 
 import scala.meta._
 import scala.collection.JavaConverters._
-class MavenMutantRunnerTest extends Stryker4sSuite with IdiomaticMockito with ArgumentMatchersSugar  {
+
+class MavenMutantRunnerTest extends Stryker4sSuite with IdiomaticMockito with ArgumentMatchersSugar {
   implicit val config: Config = Config()
   describe("runInitialTest") {
     it("should succeed on exit-code 0 invoker") {
@@ -30,7 +31,7 @@ class MavenMutantRunnerTest extends Stryker4sSuite with IdiomaticMockito with Ar
 
       val result = sut.runInitialTest(File.currentWorkingDirectory)
 
-      result should be (true)
+      result should be(true)
     }
 
     it("should fail on exit-code 1 invoker") {
@@ -42,11 +43,54 @@ class MavenMutantRunnerTest extends Stryker4sSuite with IdiomaticMockito with Ar
 
       val result = sut.runInitialTest(File.currentWorkingDirectory)
 
-      result should be (false)
+      result should be(false)
+    }
+
+    it("should not add the environment variable") {
+      val invokerMock = mock[Invoker]
+      val mockResult = mock[InvocationResult]
+      0 willBe returned by mockResult.getExitCode
+      mockResult willBe returned by invokerMock.execute(*)
+      val captor = ArgCaptor[InvocationRequest]
+      val sut = new MavenMutantRunner(new MavenProject(), invokerMock, mock[ProcessRunner], mock[SourceCollector])
+
+      val cwd = File.currentWorkingDirectory
+      val result = sut.runInitialTest(cwd)
+
+      invokerMock.execute(captor) was called
+      result should be(true)
+      val invokedRequest = captor.value
+      invokedRequest.getShellEnvironments should be(empty)
     }
   }
 
   describe("runMutants") {
+    it("should have a Killed mutant on a exit-code 1") {
+      val invokerMock = mock[Invoker]
+      val mockResult = mock[InvocationResult]
+      1 willBe returned by mockResult.getExitCode
+      mockResult willBe returned by invokerMock.execute(*)
+      val sut = new MavenMutantRunner(new MavenProject(), invokerMock, mock[ProcessRunner], mock[SourceCollector])
+
+      val cwd = File.currentWorkingDirectory
+      val result = sut.runMutant(model.Mutant(1, q">", q"<", Or), cwd, (cwd / "target/stryker4sFolder").path)
+
+      result shouldBe a[Killed]
+    }
+
+    it("should have a Survived mutant on a exit-code 0") {
+      val invokerMock = mock[Invoker]
+      val mockResult = mock[InvocationResult]
+      0 willBe returned by mockResult.getExitCode
+      mockResult willBe returned by invokerMock.execute(*)
+      val sut = new MavenMutantRunner(new MavenProject(), invokerMock, mock[ProcessRunner], mock[SourceCollector])
+
+      val cwd = File.currentWorkingDirectory
+      val result = sut.runMutant(model.Mutant(1, q">", q"<", Or), cwd, (cwd / "target/stryker4sFolder").path)
+
+      result shouldBe a[Survived]
+    }
+
     it("should add the environment variable to the request") {
       val invokerMock = mock[Invoker]
       val mockResult = mock[InvocationResult]
@@ -56,12 +100,14 @@ class MavenMutantRunnerTest extends Stryker4sSuite with IdiomaticMockito with Ar
       val sut = new MavenMutantRunner(new MavenProject(), invokerMock, mock[ProcessRunner], mock[SourceCollector])
 
       val cwd = File.currentWorkingDirectory
-      val result = sut.runMutant(model.Mutant(1, q">", q"<", Or), cwd, Paths.get("stryker4sFolder"))
+      sut.runMutant(model.Mutant(1, q">", q"<", Or), cwd, (cwd / "target/stryker4sFolder").path)
 
       invokerMock.execute(captor) was called
-      result shouldBe a[Killed]
       val invokedRequest = captor.value
-      invokedRequest.getShellEnvironments.asScala should equal (Map("ACTIVE_MUTATION" -> "1"))
+      invokedRequest.getShellEnvironments.asScala should equal(Map("ACTIVE_MUTATION" -> "1"))
+      invokedRequest.getGoals should contain only "test"
+      invokedRequest.isBatchMode should be(true)
+      invokedRequest.getProperties.getProperty("surefire.skipAfterFailureCount") should equal("1")
     }
   }
 }
