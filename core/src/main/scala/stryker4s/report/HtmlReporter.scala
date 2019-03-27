23abe306e7229d9e4cbdca041306ca6f5a893964
Hugo van Rijswijk <hugo.v.rijswijk@gmail.com>
2019-03-27 19:05:53 +0100

    Write HTML report file and add tests

--- core/src/main/scala/stryker4s/report/HtmlReporter.scala
+++ core/src/main/scala/stryker4s/report/HtmlReporter.scala
@@ -1,21 +1,28 @@
 package stryker4s.report
 import better.files.Resource
+import grizzled.slf4j.Logging
 import stryker4s.config.Config
+import stryker4s.files.FileIO
 import stryker4s.model.MutantRunResults
 import stryker4s.report.mapper.MutantRunResultMapper
 
-class HtmlReporter(implicit config: Config) extends FinishedRunReporter with MutantRunResultMapper {
+class HtmlReporter(fileIO: FileIO)(implicit config: Config)
+    extends FinishedRunReporter
+    with MutantRunResultMapper
+    with Logging {
 
   private val reportVersion = "1.0.1"
+  private val htmlReportResource =
+    s"META-INF/resources/webjars/mutation-testing-elements/$reportVersion/dist/mutation-test-elements.js"
+  private val title = "Stryker4s report"
 
   def indexHtml(json: String): String = {
-    val mutationTestElementsScript = Resource.getAsString(
-      s"META-INF/resources/webjars/mutation-testing-elements/$reportVersion/dist/mutation-test-elements.js")
+    val mutationTestElementsScript = fileIO.readResource(htmlReportResource)
 
     s"""<!DOCTYPE html>
        |<html>
        |<body>
-       |  <mutation-test-report-app title-postfix="Stryker4s report"></mutation-test-report-app>
+       |  <mutation-test-report-app title-postfix="$title"></mutation-test-report-app>
        |  <script>
        |    document.querySelector('mutation-test-report-app').report = $json
        |  </script>
@@ -29,6 +36,10 @@ class HtmlReporter(implicit config: Config) extends FinishedRunReporter with Mut
   override def reportRunFinished(runResults: MutantRunResults): Unit = {
     val mapped = toReport(runResults).toJson
 
-    println(mapped)
+    val targetLocation = config.baseDir / s"target/stryker4s-report-${System.currentTimeMillis()}" / "index.html"
+    val reportContent = indexHtml(mapped)
+    fileIO.createAndWrite(targetLocation, reportContent)
+
+    debug(s"Written HTML report to $targetLocation")
   }
 }
