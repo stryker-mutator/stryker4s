7ed0c55b7e1f9ceca9b029d695c3f879dafe4481
Hugo van Rijswijk <hugo.v.rijswijk@gmail.com>
2019-04-02 13:42:49 +0200

    Add test for debug message

--- core/src/test/scala/stryker4s/report/HtmlReporterTest.scala
+++ core/src/test/scala/stryker4s/report/HtmlReporterTest.scala
@@ -5,12 +5,13 @@ import org.mockito.{ArgumentMatchersSugar, MockitoSugar}
 import stryker4s.config.Config
 import stryker4s.files.{DiskFileIO, FileIO}
 import stryker4s.model.MutantRunResults
+import stryker4s.scalatest.LogMatchers
 import stryker4s.testutil.Stryker4sSuite
 
 import scala.concurrent.duration._
 import scala.io.Source
 
-class HtmlReporterTest extends Stryker4sSuite with MockitoSugar with ArgumentMatchersSugar {
+class HtmlReporterTest extends Stryker4sSuite with MockitoSugar with ArgumentMatchersSugar with LogMatchers {
 
   describe("indexHtml") {
     it("should contain title") {
@@ -62,6 +63,17 @@ class HtmlReporterTest extends Stryker4sSuite with MockitoSugar with ArgumentMat
       val fileCaptor = ArgCaptor[File]
       verify(mockFileIO).createAndWrite(fileCaptor, any[Iterator[Char]])
       fileCaptor.value.pathAsString should fullyMatch regex ".*target(/|\\\\)stryker4s-report-(\\d*)(/|\\\\)index.html$"
+    }
+
+    it("should debug log a message") {
+      implicit val config: Config = Config()
+      val mockFileIO = mock[FileIO]
+      val sut = new HtmlReporter(mockFileIO)
+      val runResults = MutantRunResults(Nil, 50.0, 30.seconds)
+
+      sut.reportRunFinished(runResults)
+
+      "Written HTML report to " shouldBe loggedAsDebug
     }
   }
 }
