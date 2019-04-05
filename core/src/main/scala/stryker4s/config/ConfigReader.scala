559188e4ca34d32c437b6c98a2eff6f098fea1fb
Hugo van Rijswijk <hugo.v.rijswijk@gmail.com>
2019-04-05 13:12:58 +0200

    Small cleanup for configreader

--- core/src/main/scala/stryker4s/config/ConfigReader.scala
+++ core/src/main/scala/stryker4s/config/ConfigReader.scala
@@ -15,20 +15,14 @@ object ConfigReader extends ConfigReaderImplicits with Logging {
 
   /** Read config from stryker4s.conf. Or use the default Config if no config file is found.
     */
-  def readConfig(confFile: File = defaultConfigFileLocation): Config = {
-    readConfigOfType[Config](confFile) match {
-      case Left(failures) => tryRecoverFromFailures(failures)
-      case Right(config) =>
-        info("Using stryker4s.conf in the current working directory")
-
-        config
-    }
-  }
+  def readConfig(confFile: File = defaultConfigFileLocation): Config = readConfigOfType[Config](confFile) fold (
+    tryRecoverFromFailures,
+    logAndReturn(info( "Using stryker4s.conf in the current working directory"))
+  )
 
   def readConfigOfType[T](confFile: File = defaultConfigFileLocation)(
-      implicit derivation: Derivation[PureConfigReader[T]]): Either[ConfigReaderFailures, T] = {
+      implicit derivation: Derivation[PureConfigReader[T]]): Either[ConfigReaderFailures, T] =
     pureconfig.loadConfig[T](confFile.path, namespace = "stryker4s")
-  }
 
   private def tryRecoverFromFailures[T](failures: ConfigReaderFailures): Config = failures match {
     case ConfigReaderFailures(CannotReadFile(fileName, Some(_: FileNotFoundException)), _) =>
@@ -44,5 +38,10 @@ object ConfigReader extends ConfigReaderImplicits with Logging {
       error("Failures in reading config: ")
       error(failures.toList.map(_.description).mkString(System.lineSeparator))
       throw ConfigReaderException(failures)
+  }
+
+  def logAndReturn[T](log: => Unit)(obj: T): T = {
+    log
+    obj
   }
 }
