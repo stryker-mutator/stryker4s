638f39920450bfcfd36778194063441e7744c0b9
Hugo van Rijswijk <hugo.v.rijswijk@gmail.com>
2019-05-08 12:24:20 +0200

    Write mutation-test-elements.js to separate file
    Also log html file location to info and clean up tests a bit

--- core/src/main/scala/stryker4s/report/HtmlReporter.scala
+++ core/src/main/scala/stryker4s/report/HtmlReporter.scala
@@ -1,56 +1,57 @@
 package stryker4s.report
+
 import grizzled.slf4j.Logging
 import stryker4s.config.Config
 import stryker4s.files.FileIO
 import stryker4s.model.MutantRunResults
 import stryker4s.report.mapper.MutantRunResultMapper
 
+import scala.io.Source
+
 class HtmlReporter(fileIO: FileIO)(implicit config: Config)
     extends FinishedRunReporter
     with MutantRunResultMapper
     with Logging {
 
-  private val htmlReportResource = s"mutation-testing-elements/mutation-test-elements.js"
   private val title = "Stryker4s report"
+  private val mutationTestElementsName = "mutation-test-elements.js"
+  private val htmlReportResource = s"mutation-testing-elements/$mutationTestElementsName"
   private val reportFilename = "report.js"
 
-  def indexHtml(): Iterator[Char] = {
-    val mutationTestElementsScript = fileIO.readResource(htmlReportResource)
-
-    val startHtml = s"""<!DOCTYPE html>
+  val indexHtml: String =
+    s"""<!DOCTYPE html>
        |<html>
+       |<head>
+       |  <script src="mutation-test-elements.js"></script>
+       |</head>
        |<body>
        |  <mutation-test-report-app title-postfix="$title">
        |    Your browser doesn't support <a href="https://caniuse.com/#search=custom%20elements">custom elements</a>.
        |    Please use a latest version of an evergreen browser (Firefox, Chrome, Safari, Opera, etc).
        |  </mutation-test-report-app>
        |  <script src="$reportFilename"></script>
-       |  <script>
-       |    """.stripMargin
-    val endHtml = s"""
-       |  </script>
        |</body>
        |</html>""".stripMargin
 
-    startHtml.iterator ++
-      mutationTestElementsScript ++
-      endHtml
-  }
+  def reportJs(json: String): String = s"document.querySelector('mutation-test-report-app').report = $json"
 
-  def reportJs(json: String): String =
-    s"document.querySelector('mutation-test-report-app').report = $json"
+  def testElementsJs(): Source = fileIO.readResource(htmlReportResource)
 
   override def reportRunFinished(runResults: MutantRunResults): Unit = {
     val mapped = toReport(runResults).toJson
 
     val targetLocation = config.baseDir / s"target/stryker4s-report-${System.currentTimeMillis()}"
+    val mutationTestElementsLocation = targetLocation / mutationTestElementsName
     val indexLocation = targetLocation / "index.html"
     val reportLocation = targetLocation / reportFilename
-    val indexContent = indexHtml()
-    val reportContent = reportJs(mapped)
-    fileIO.createAndWrite(indexLocation, indexContent)
-    fileIO.createAndWrite(reportLocation, reportContent)
 
-    debug(s"Written HTML report to $targetLocation")
+    val reportContent = reportJs(mapped)
+    val mutationTestElementsContent = testElementsJs()
+
+    fileIO.createAndWrite(indexLocation, indexHtml)
+    fileIO.createAndWrite(reportLocation, reportContent)
+    fileIO.createAndWrite(mutationTestElementsLocation, mutationTestElementsContent)
+
+    info(s"Written HTML report to $targetLocation")
   }
 }
--- core/src/test/scala/stryker4s/report/HtmlReporterTest.scala
+++ core/src/test/scala/stryker4s/report/HtmlReporterTest.scala
@@ -17,25 +17,24 @@ class HtmlReporterTest extends Stryker4sSuite with MockitoSugar with ArgumentMat
     it("should contain title") {
       implicit val config: Config = Config()
       val mockFileIO = mock[FileIO]
-      val resourceLocation = "mutation-testing-elements/mutation-test-elements.js"
-      when(mockFileIO.readResource(resourceLocation)) thenReturn Source.fromString("console.log('hello');")
       val sut = new HtmlReporter(mockFileIO)
 
-      val result = sut.indexHtml()
+      val result = sut.indexHtml
 
-      val expected = s"""<!DOCTYPE html>
-                        |<html>
-                        |<body>
-                        |  <mutation-test-report-app title-postfix="Stryker4s report">
-                        |    Your browser doesn't support <a href="https://caniuse.com/#search=custom%20elements">custom elements</a>.
-                        |    Please use a latest version of an evergreen browser (Firefox, Chrome, Safari, Opera, etc).
-                        |  </mutation-test-report-app>
-                        |  <script src="report.js"></script>
-                        |  <script>
-                        |    console.log('hello');
-                        |  </script>
-                        |</body>
-                        |</html>""".stripMargin
+      val expected =
+        """<!DOCTYPE html>
+          |<html>
+          |<head>
+          |  <script src="mutation-test-elements.js"></script>
+          |</head>
+          |<body>
+          |  <mutation-test-report-app title-postfix="Stryker4s report">
+          |    Your browser doesn't support <a href="https://caniuse.com/#search=custom%20elements">custom elements</a>.
+          |    Please use a latest version of an evergreen browser (Firefox, Chrome, Safari, Opera, etc).
+          |  </mutation-test-report-app>
+          |  <script src="report.js"></script>
+          |</body>
+          |</html>""".stripMargin
       result.mkString should equal(expected)
     }
   }
@@ -46,9 +45,9 @@ class HtmlReporterTest extends Stryker4sSuite with MockitoSugar with ArgumentMat
       val mockFileIO = mock[FileIO]
       val sut = new HtmlReporter(mockFileIO)
 
-      val result = sut.reportJs("""{ 'foo': 'bar' }""")
+      val result = sut.reportJs("{ 'foo': 'bar' }")
 
-      val expected = s"""document.querySelector('mutation-test-report-app').report = { 'foo': 'bar' }"""
+      val expected = "document.querySelector('mutation-test-report-app').report = { 'foo': 'bar' }"
       result.mkString should equal(expected)
 
     }
@@ -61,41 +60,54 @@ class HtmlReporterTest extends Stryker4sSuite with MockitoSugar with ArgumentMat
 
       val sut = new HtmlReporter(fileIO)
 
-      val result = sut.indexHtml()
+      val result = sut.testElementsJs()
       result.mkString.length should be > 50
     }
   }
 
   describe("reportRunFinished") {
+    implicit val config: Config = Config()
+    val stryker4sReportFolderRegex = ".*target(/|\\\\)stryker4s-report-(\\d*)(/|\\\\)[a-z]*\\.[a-z]*$"
+
     it("should write the report files to the report directory") {
-      implicit val config: Config = Config()
       val mockFileIO = mock[FileIO]
       val sut = new HtmlReporter(mockFileIO)
       val runResults = MutantRunResults(Nil, 50.0, 30.seconds)
 
       sut.reportRunFinished(runResults)
 
-      val indexCaptor = ArgCaptor[File]
-      val reportCaptor = ArgCaptor[File]
-      verify(mockFileIO).createAndWrite(indexCaptor, any[Iterator[Char]])
-      verify(mockFileIO).createAndWrite(reportCaptor, any[String])
-      val paths = List(indexCaptor.value, reportCaptor.value).map(_.pathAsString)
+      val writtenFilesCaptor = ArgCaptor[File]
 
-      // ends with target/stryker4s-report-$TIMESTAMP/filename.extension
-      all(paths) should fullyMatch regex ".*target(/|\\\\)stryker4s-report-(\\d*)(/|\\\\)[a-z]*\\.[a-z]*$"
-      indexCaptor.value.name should be("index.html")
-      reportCaptor.value.name should be("report.js")
+      verify(mockFileIO, times(2)).createAndWrite(writtenFilesCaptor, any[String])
+
+      val paths = writtenFilesCaptor.values.map(_.pathAsString)
+      all(paths) should fullyMatch regex stryker4sReportFolderRegex
+
+      writtenFilesCaptor.values.map(_.name) should contain only ("index.html", "report.js")
+    }
+
+    it("should write the mutation-test-elements.js file to the report directory") {
+      val mockFileIO = mock[FileIO]
+      val sut = new HtmlReporter(mockFileIO)
+      val runResults = MutantRunResults(Nil, 50.0, 30.seconds)
+
+      sut.reportRunFinished(runResults)
+
+      val elementsCaptor = ArgCaptor[File]
+      verify(mockFileIO).createAndWrite(elementsCaptor, any[Source])
+
+      elementsCaptor.value.pathAsString should fullyMatch regex stryker4sReportFolderRegex
+      elementsCaptor.value.name equals "mutation-test-elements.js"
     }
 
     it("should debug log a message") {
-      implicit val config: Config = Config()
       val mockFileIO = mock[FileIO]
       val sut = new HtmlReporter(mockFileIO)
       val runResults = MutantRunResults(Nil, 50.0, 30.seconds)
 
       sut.reportRunFinished(runResults)
 
-      "Written HTML report to " shouldBe loggedAsDebug
+      "Written HTML report to " shouldBe loggedAsInfo
     }
   }
 }
