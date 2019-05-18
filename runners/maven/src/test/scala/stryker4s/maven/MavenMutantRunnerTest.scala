8e5f44a6227051a336f95290d3cccc8233c125bb
Hugo van Rijswijk <hugo.v.rijswijk@gmail.com>
2019-05-18 09:57:41 +0200

    Unify mockito style in mvn project

--- runners/maven/src/test/scala/stryker4s/maven/MavenMutantRunnerTest.scala
+++ runners/maven/src/test/scala/stryker4s/maven/MavenMutantRunnerTest.scala
@@ -4,7 +4,7 @@ import better.files._
 import org.apache.maven.project.MavenProject
 import org.apache.maven.shared.invoker._
 import org.mockito.captor.ArgCaptor
-import org.mockito.{ArgumentMatchersSugar, IdiomaticMockito}
+import org.mockito.scalatest.MockitoSugar
 import stryker4s.config.Config
 import stryker4s.extension.mutationtype.LesserThan
 import stryker4s.maven.runner.MavenMutantRunner
@@ -17,47 +17,50 @@ import stryker4s.testutil.Stryker4sSuite
 import scala.collection.JavaConverters._
 import scala.meta._
 
-class MavenMutantRunnerTest extends Stryker4sSuite with IdiomaticMockito with ArgumentMatchersSugar {
+class MavenMutantRunnerTest extends Stryker4sSuite with MockitoSugar {
   implicit val config: Config = Config()
   describe("runInitialTest") {
     it("should succeed on exit-code 0 invoker") {
       val invokerMock = mock[Invoker]
       val mockResult = mock[InvocationResult]
 
-      0 willBe returned by mockResult.getExitCode
-      mockResult willBe returned by invokerMock.execute(*)
+      when(mockResult.getExitCode).thenReturn(0)
+      when(invokerMock.execute(any)).thenReturn(mockResult)
       val sut = new MavenMutantRunner(new MavenProject(), invokerMock, mock[SourceCollector], mock[Reporter])
 
       val result = sut.runInitialTest(File.currentWorkingDirectory)
 
+      verify(invokerMock).setWorkingDirectory(any)
       result should be(true)
     }
 
     it("should fail on exit-code 1 invoker") {
       val invokerMock = mock[Invoker]
       val mockResult = mock[InvocationResult]
-      1 willBe returned by mockResult.getExitCode
-      mockResult willBe returned by invokerMock.execute(*)
+      when(mockResult.getExitCode).thenReturn(1)
+      when(invokerMock.execute(any)).thenReturn(mockResult)
       val sut = new MavenMutantRunner(new MavenProject(), invokerMock, mock[SourceCollector], mock[Reporter])
 
       val result = sut.runInitialTest(File.currentWorkingDirectory)
 
+      verify(invokerMock).setWorkingDirectory(any)
       result should be(false)
     }
 
     it("should not add the environment variable") {
       val invokerMock = mock[Invoker]
       val mockResult = mock[InvocationResult]
-      0 willBe returned by mockResult.getExitCode
-      mockResult willBe returned by invokerMock.execute(*)
+      when(mockResult.getExitCode).thenReturn(0)
+      when(invokerMock.execute(any)).thenReturn(mockResult)
       val captor = ArgCaptor[InvocationRequest]
       val sut = new MavenMutantRunner(new MavenProject(), invokerMock, mock[SourceCollector], mock[Reporter])
 
       val cwd = File.currentWorkingDirectory
       val result = sut.runInitialTest(cwd)
 
-      invokerMock.execute(captor) was called
       result should be(true)
+      verify(invokerMock).setWorkingDirectory(any)
+      verify(invokerMock).execute(captor)
       val invokedRequest = captor.value
       invokedRequest.getShellEnvironments should be(empty)
     }
@@ -67,41 +70,44 @@ class MavenMutantRunnerTest extends Stryker4sSuite with IdiomaticMockito with Ar
     it("should have a Killed mutant on a exit-code 1") {
       val invokerMock = mock[Invoker]
       val mockResult = mock[InvocationResult]
-      1 willBe returned by mockResult.getExitCode
-      mockResult willBe returned by invokerMock.execute(*)
+      when(mockResult.getExitCode).thenReturn(1)
+      when(invokerMock.execute(any)).thenReturn(mockResult)
       val sut = new MavenMutantRunner(new MavenProject(), invokerMock, mock[SourceCollector], mock[Reporter])
 
       val cwd = File.currentWorkingDirectory
       val result = sut.runMutant(model.Mutant(1, q">", q"<", LesserThan), cwd)(cwd.path)
 
+      verify(invokerMock).setWorkingDirectory(any)
       result shouldBe a[Killed]
     }
 
     it("should have a Survived mutant on a exit-code 0") {
       val invokerMock = mock[Invoker]
       val mockResult = mock[InvocationResult]
-      0 willBe returned by mockResult.getExitCode
-      mockResult willBe returned by invokerMock.execute(*)
+      when(mockResult.getExitCode).thenReturn(0)
+      when(invokerMock.execute(any)).thenReturn(mockResult)
       val sut = new MavenMutantRunner(new MavenProject(), invokerMock, mock[SourceCollector], mock[Reporter])
 
       val cwd = File.currentWorkingDirectory
       val result = sut.runMutant(model.Mutant(1, q">", q"<", LesserThan), cwd)(cwd.path)
 
+      verify(invokerMock).setWorkingDirectory(any)
       result shouldBe a[Survived]
     }
 
     it("should add the environment variable to the request") {
       val invokerMock = mock[Invoker]
       val mockResult = mock[InvocationResult]
-      1 willBe returned by mockResult.getExitCode
-      mockResult willBe returned by invokerMock.execute(*)
+      when(mockResult.getExitCode).thenReturn(1)
+      when(invokerMock.execute(any)).thenReturn(mockResult)
       val captor = ArgCaptor[InvocationRequest]
       val sut = new MavenMutantRunner(new MavenProject(), invokerMock, mock[SourceCollector], mock[Reporter])
 
       val cwd = File.currentWorkingDirectory
       sut.runMutant(model.Mutant(1, q">", q"<", LesserThan), cwd)
 
-      invokerMock.execute(captor) was called
+      verify(invokerMock).setWorkingDirectory(any)
+      verify(invokerMock).execute(captor)
       val invokedRequest = captor.value
       invokedRequest.getShellEnvironments.asScala should equal(Map("ACTIVE_MUTATION" -> "1"))
       invokedRequest.getGoals should contain only "test"
