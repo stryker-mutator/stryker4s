fd32728a03b8d3fcf0fd451c5bfdc41c7326d470
Scala Steward <me@scala-steward.org>
2019-06-17 13:25:49 +0200

    Update mutation-testing-report-schema to 1.0.8

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,7 +15,7 @@ object Dependencies {
     val everitJsonSchema = "1.11.1"
     val circe = "0.11.1"
     val mutationTestingElements = "1.0.7"
-    val mutationTestingSchema = "1.0.5"
+    val mutationTestingSchema = "1.0.8"
     val scalajHttp = "2.4.2"
   }
 
