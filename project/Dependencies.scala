daa5609187ca4951ed769ee245f1edf5621f8e9a
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2019-09-23 07:14:23 +0200

    Update mockito-scala-scalatest to 1.5.17 (#265)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
     val scalameta = "4.2.3"
     val pureconfig = "0.11.1"
     val scalatest = "3.0.8"
-    val mockitoScala = "1.5.16"
+    val mockitoScala = "1.5.17"
     val betterFiles = "3.8.0"
     val log4j = "2.12.1"
     val grizzledSlf4j = "1.3.4"
