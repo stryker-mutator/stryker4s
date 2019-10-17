879164f1da18e6622cc1888ce36f25b990cbd407
Marco Kuper <MarcoKuper42@gmail.com>
2019-10-17 21:19:36 +0200

    group failure recovery, introduce cats either syntax

--- core/src/main/scala/stryker4s/config/ConfigReader.scala
+++ core/src/main/scala/stryker4s/config/ConfigReader.scala
@@ -12,6 +12,8 @@ import pureconfig.generic.auto._
 
 object ConfigReader extends ConfigReaderImplicits with Logging {
 
+  import EitherSyntax.EitherOps
+
   val defaultConfigFileLocation: File = File.currentWorkingDirectory / "stryker4s.conf"
   private val configDocUrl: String =
     "https://github.com/stryker-mutator/stryker4s/blob/master/docs/CONFIGURATION.md"
@@ -20,7 +22,14 @@ object ConfigReader extends ConfigReaderImplicits with Logging {
     */
   def readConfig(confFile: File = defaultConfigFileLocation): Config = {
     implicit val hint: ProductHint[Config] = ProductHint[Config](allowUnknownKeys = false)
-    def recoverUnknownKey: PartialFunction[ConfigReaderFailures, Config] = {
+
+    recoverAndLog(
+      readConfigOfType[Config](confFile).recoverWith(Failure.recoverUnknownKey(confFile))
+    )
+  }
+
+  object Failure {
+    def recoverUnknownKey(confFile: File): PartialFunction[ConfigReaderFailures, Config] = {
       case ConfigReaderFailures(ConvertFailure(UnknownKey(key), _, _), failures) =>
         val unknownKeys = key :: failures.collect {
           case ConvertFailure(UnknownKey(k), _, _) => k
@@ -30,9 +39,24 @@ object ConfigReader extends ConfigReaderImplicits with Logging {
         nonStrictlyReadConfig(confFile)
     }
 
-    recoverAndLog(
-      recoverWith(readConfigOfType[Config](confFile))(recoverUnknownKey)
-    )
+    def recoverFileNotFound: PartialFunction[ConfigReaderFailures, Config] = {
+      case ConfigReaderFailures(CannotReadFile(fileName, Some(_: FileNotFoundException)), _) =>
+        warn(s"Could not find config file $fileName")
+        warn("Using default config instead...")
+        // FIXME: sbt has its own (older) dependency on Typesafe config, which causes an error with Pureconfig when running the sbt plugin
+        //  If that's fixed we can add this again
+        //  https://github.com/stryker-mutator/stryker4s/issues/116
+        // info("Config used: " + defaultConf.toHoconString)
+
+        Config()
+    }
+
+    def throwException: PartialFunction[ConfigReaderFailures, Config] = {
+      case failures =>
+        error("Failures in reading config: ")
+        error(failures.toList.map(_.description).mkString(System.lineSeparator))
+        throw ConfigReaderException(failures)
+    }
   }
 
   def readConfigOfType[T](
@@ -40,40 +64,33 @@ object ConfigReader extends ConfigReaderImplicits with Logging {
   )(implicit derivation: Derivation[PureConfigReader[T]]): Either[ConfigReaderFailures, T] =
     ConfigSource.file(confFile.path).at("stryker4s").load[T]
 
-  private def recoverAndLog(either: Either[ConfigReaderFailures, Config]): Config =
-    either.fold(
-      tryRecoverFromFailures,
-      logAndReturn(info("Using stryker4s.conf in the current working directory"))
-    )
+  private def recoverAndLog(either: Either[ConfigReaderFailures, Config]): Config = {
+    val conf = either.valueOr(Failure.recoverFileNotFound.orElse(Failure.throwException))
+    logAndReturn(info("Using stryker4s.conf in the current working directory"))(conf)
+  }
 
   private def nonStrictlyReadConfig(confFile: File): Config =
     recoverAndLog(
       readConfigOfType[Config](confFile)
     )
 
-  private def tryRecoverFromFailures(failures: ConfigReaderFailures): Config = failures match {
-    case ConfigReaderFailures(CannotReadFile(fileName, Some(_: FileNotFoundException)), _) =>
-      warn(s"Could not find config file $fileName")
-      warn("Using default config instead...")
-      // FIXME: sbt has its own (older) dependency on Typesafe config, which causes an error with Pureconfig when running the sbt plugin
-      //  If that's fixed we can add this again
-      //  https://github.com/stryker-mutator/stryker4s/issues/116
-      // info("Config used: " + defaultConf.toHoconString)
-
-      Config()
-    case _ =>
-      error("Failures in reading config: ")
-      error(failures.toList.map(_.description).mkString(System.lineSeparator))
-      throw ConfigReaderException(failures)
-  }
-
-  private def recoverWith[A, B, BB >: B](eab: Either[A, B])(pf: PartialFunction[A, BB]): Either[A, BB] = eab match {
-    case Left(a) if pf.isDefinedAt(a) => Right(pf(a))
-    case _                            => eab
-  }
-
   def logAndReturn[T](log: => Unit)(obj: T): T = {
     log
     obj
+  }
+}
+
+object EitherSyntax {
+
+  implicit final class EitherOps[A, B](val eab: Either[A, B]) extends AnyVal {
+    def recoverWith[BB >: B](pf: PartialFunction[A, BB]): Either[A, BB] = eab match {
+      case Left(a) if pf.isDefinedAt(a) => Right(pf(a))
+      case _                            => eab
+    }
+
+    def valueOr[BB >: B](f: A => BB): BB = eab match {
+      case Left(a)  => f(a)
+      case Right(b) => b
+    }
   }
 }
