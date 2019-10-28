3d3ca0fed1faeddf6318dde3b35b331046dc4adf
Marco Kuper <MarcoKuper42@gmail.com>
2019-10-28 20:49:40 +0100

    improve logging for unknown keys

--- core/src/main/scala/stryker4s/config/ConfigReader.scala
+++ core/src/main/scala/stryker4s/config/ConfigReader.scala
@@ -115,7 +115,8 @@ object ConfigReader extends ConfigReaderImplicits with Logging {
         }
 
         warn(
-          s"The following configuration keys are not used: ${unknownKeys.mkString(", ")}.\n" +
+          s"The following configuration keys are not used, maybe the stem from an older " +
+             s"stryker4s version: ${unknownKeys.mkString(", ")}.\n" +
             s"Please check the documentation at $configDocUrl for available options."
         )
         implicit val hint: ProductHint[Config] = ProductHint[Config](allowUnknownKeys = true)
