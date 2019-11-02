5bb5bfd2d6e2f898dc69bbc10f9cb0e8cfffbafe
Scala Steward <me@scala-steward.org>
2019-11-02 23:36:11 +0100

    Update mutation-testing-elements to 1.2.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,7 +13,7 @@ object Dependencies {
     val grizzledSlf4j = "1.3.4"
     val cats = "2.0.0"
     val circe = "0.12.3"
-    val mutationTestingElements = "1.1.1"
+    val mutationTestingElements = "1.2.0"
     val mutationTestingMetrics = "1.2.0"
     val scalajHttp = "2.4.2"
   }
