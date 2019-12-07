c7c181f98f1e6483c48affd6162e366900d5b737
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2019-12-07 10:13:05 +0100

    Update mockito-scala-scalatest to 1.10.0 (#325)
    Update mockito-scala-scalatest to 1.10.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
     val scalameta = "4.3.0"
     val pureconfig = "0.12.1"
     val scalatest = "3.1.0"
-    val mockitoScala = "1.7.1"
+    val mockitoScala = "1.10.0"
     val betterFiles = "3.8.0"
     val log4j = "2.12.1"
     val grizzledSlf4j = "1.3.4"
