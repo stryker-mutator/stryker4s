c497f087990806495b801b6b5a1bacade7c466cf
Scala Steward <me@scala-steward.org>
2019-12-18 05:47:25 +0100

    Update mockito-scala-scalatest to 1.10.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
     val scalameta = "4.3.0"
     val pureconfig = "0.12.1"
     val scalatest = "3.1.0"
-    val mockitoScala = "1.10.0"
+    val mockitoScala = "1.10.1"
     val betterFiles = "3.8.0"
     val log4j = "2.13.0"
     val grizzledSlf4j = "1.3.4"
