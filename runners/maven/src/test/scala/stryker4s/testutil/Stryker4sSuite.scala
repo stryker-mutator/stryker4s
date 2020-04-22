4cf004202b57bf6ea54ca4299268a898e0420176
Hugo van Rijswijk <hugo.v.rijswijk@gmail.com>
2020-04-22 13:11:22 +0200

    Update Stryker4sSuite.scala

--- runners/maven/src/test/scala/stryker4s/testutil/Stryker4sSuite.scala
+++ runners/maven/src/test/scala/stryker4s/testutil/Stryker4sSuite.scala
@@ -4,4 +4,4 @@ import org.scalatest.matchers.should.Matchers
 import org.scalatest.funspec.AnyFunSpec
 import org.scalatest.{LoneElement, OptionValues}
 
-trait Stryker4sSuite extends AnyFunSpec with Matchers with OptionValues with LoneElement
+ abstract class Stryker4sSuite extends AnyFunSpec with Matchers with OptionValues with LoneElement
