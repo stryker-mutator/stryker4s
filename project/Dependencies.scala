12bda04fc1da3de775d9521e48a0f05a5c1bd8a1
Hugo van Rijswijk <hugo.v.rijswijk@gmail.com>
2020-04-23 08:43:55 +0200

    Update to Scala 2.13.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
   object versions {
     val scala212 = "2.12.11"
-    val scala213 = "2.13.1"
+    val scala213 = "2.13.2"
 
     val scalameta = "4.3.8"
     val pureconfig = "0.12.3"
