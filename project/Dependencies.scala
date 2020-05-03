64d881fa4bbca5c59695fc72d14bba319608ff34
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-05-03 18:38:29 +0200

    Update sttp to 2.1.0 (#443)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -16,7 +16,7 @@ object Dependencies {
     val circe = "0.13.0"
     val mutationTestingElements = "1.3.1"
     val mutationTestingMetrics = "1.3.1"
-    val sttp = "2.0.9"
+    val sttp = "2.1.0"
   }
 
   object test {
