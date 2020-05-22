8c49c71bf51d708aa752eff0e6aa94ee2fdee591
Scala Steward <me@scala-steward.org>
2020-05-22 17:04:13 +0200

    Update circe to 2.1.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -17,7 +17,7 @@ object Dependencies {
     val circe = "0.13.0"
     val mutationTestingElements = "1.3.1"
     val mutationTestingMetrics = "1.3.1"
-    val sttp = "2.1.2"
+    val sttp = "2.1.4"
   }
 
   object test {
