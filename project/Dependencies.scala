48bad8b8aa45bcb7f88bafa7edaa75b6809f8a8a
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-06-03 21:33:04 +0200

    Update scalameta to 4.3.14 (#494)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
     val scala213 = "2.13.2"
     val crossScalaVersions = Seq(scala213, scala212)
 
-    val scalameta = "4.3.13"
+    val scalameta = "4.3.14"
     val pureconfig = "0.12.3"
     val scalatest = "3.1.2"
     val mockitoScala = "1.14.4"
