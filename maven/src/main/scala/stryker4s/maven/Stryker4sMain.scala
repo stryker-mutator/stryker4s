bc7ea956b4c991345e003930e3e79e1db69a5749
Hugo van Rijswijk <hugo.van.rijswijk@nn.nl>
2020-06-24 18:57:21 +0200

    Add contextShift to maven main

--- maven/src/main/scala/stryker4s/maven/Stryker4sMain.scala
+++ maven/src/main/scala/stryker4s/maven/Stryker4sMain.scala
@@ -6,12 +6,14 @@ import org.apache.maven.plugins.annotations.{Mojo, Parameter}
 import org.apache.maven.project.MavenProject
 import stryker4s.run.threshold.ErrorStatus
 import scala.concurrent.ExecutionContext.Implicits.global
+import cats.effect.IO
 
 /** The main goal for this plugin. Starts Stryker4s.
   */
 @Mojo(name = "run")
 class Stryker4sMain @Inject() (@Parameter(defaultValue = "${project}") project: MavenProject) extends AbstractMojo {
   override def execute(): Unit = {
+    implicit val cs: ContextShift[IO] = IO.contextShift(implicitly[ExecutionContext])
     new Stryker4sMavenRunner(project).run() match {
       case ErrorStatus => throw new MojoFailureException("Mutation score was below configured threshold")
       case _           =>
