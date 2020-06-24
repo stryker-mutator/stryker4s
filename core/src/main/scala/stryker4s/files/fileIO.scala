86cc6771d13c0c5a4b6a8cfbc8d3c2d7175a2489
Hugo van Rijswijk <hugo.van.rijswijk@nn.nl>
2020-06-24 18:09:38 +0200

    Use fs2 file IO

--- core/src/main/scala/stryker4s/files/fileIO.scala
+++ core/src/main/scala/stryker4s/files/fileIO.scala
@@ -1,30 +1,37 @@
 package stryker4s.files
 import better.files._
 import cats.effect.IO
-
+import fs2._
+import fs2.io.readInputStream
+import fs2.io.file._
+import cats.effect.Blocker
+import cats.effect.ContextShift
+import cats.effect.Sync
 sealed trait FileIO {
   def createAndWriteFromResource(file: File, resource: String): IO[Unit]
 
   def createAndWrite(file: File, content: String): IO[Unit]
 }
 
-class DiskFileIO() extends FileIO {
-
-  // TODO: Replace with fs2 writing
+class DiskFileIO()(implicit cs: ContextShift[IO], s: Sync[IO]) extends FileIO {
   override def createAndWriteFromResource(file: File, resourceName: String): IO[Unit] =
-    IO {
-      file.createFileIfNotExists(createParents = true)
+    Blocker[IO].use { blocker =>
+      val stream = IO { this.getClass().getResourceAsStream(resourceName) }
 
-      for {
-        in <- Resource.getAsStream(resourceName).autoClosed
-        out <- file.newOutputStream.autoClosed
-      } in pipeTo out
+      readInputStream(stream, 8192, blocker)
+        .through(writeAll(file.path, blocker))
+        .compile
+        .drain
     }
 
   override def createAndWrite(file: File, content: String): IO[Unit] =
-    IO {
-      file.createFileIfNotExists(createParents = true)
-      file.writeText(content)
-      ()
+    Blocker[IO].use { blocker =>
+      createDirectories(blocker, file.parent.path).flatMap { _ =>
+        Stream(content)
+          .through(text.utf8Encode)
+          .through(writeAll(file.path, blocker))
+          .compile
+          .drain
+      }
     }
 }
