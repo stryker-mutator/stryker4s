6c974a3fd18c1df9d9c08255fba11a1b8d13d258
Eugene Platonov <jozic@live.com>
2020-08-11 09:59:56 -0400

    Propagate active profiles from Maven plugin configuration to stryker runs (#550)

--- maven/src/main/scala/stryker4s/maven/runner/MavenMutantRunner.scala
+++ maven/src/main/scala/stryker4s/maven/runner/MavenMutantRunner.scala
@@ -55,6 +55,7 @@ class MavenMutantRunner(project: MavenProject, invoker: Invoker, sourceCollector
       .setOutputHandler(debug(_))
       .setBatchMode(true)
       .setProperties(context.properties)
+      .setProfiles(project.getActiveProfiles.asScala.map(_.getId).asJava)
 
   private def createRequestWithMutation(mutant: Mutant, context: Context): InvocationRequest =
     createRequest(context)
