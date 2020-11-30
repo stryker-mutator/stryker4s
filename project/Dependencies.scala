4ebd2edc45d67cdcd721e92d28ee43460b8f2498
Hugo van Rijswijk <hugo.v.rijswijk@gmail.com>
2020-11-30 11:37:33 +0100

    Update Scala 3 to 3.0.0-M2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
     val scala211 = "2.11.12"
     val scala212 = "2.12.12"
     val scala213 = "2.13.4"
-    val scala3 = "3.0.0-M1"
+    val scala3 = "3.0.0-M2"
 
     /** Cross-versions for main projects
       */
