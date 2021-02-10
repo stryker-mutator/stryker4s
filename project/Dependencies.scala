7fe5eee97b5c6079b64587a817235814fd07dd27
Scala Steward <me@scala-steward.org>
2021-02-10 02:20:03 +0100

    Update cats-core to 2.4.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -22,8 +22,8 @@ object Dependencies {
 
     // Direct dependencies
     val betterFiles = "3.9.1"
-    val catsCore = "2.3.1"
-    val catsEffect = "2.3.1"
+    val catsCore = "2.4.0"
+    val catsEffect = "2.4.0"
     val circe = "0.13.0"
     val fs2 = "2.5.0"
     val log4j = "2.14.0"
