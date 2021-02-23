47f38d838ca3a431b959df3d6b06fba367a1b125
Hugo van Rijswijk <hugo.v.rijswijk@gmail.com>
2021-02-23 12:31:02 +0100

    Update Scala 2.13 to 2.13.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
   object versions {
     val scala211 = "2.11.12"
     val scala212 = "2.12.13"
-    val scala213 = "2.13.4"
+    val scala213 = "2.13.5"
     val scala3 = "3.0.0-RC1"
 
     /** Cross-versions for main projects
