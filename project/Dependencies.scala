34ebdc36359779834d01a24f74b51b3bdc295777
Scala Steward <me@scala-steward.org>
2021-03-29 21:19:21 +0200

    Update fs2-core, fs2-io to 3.0.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -25,7 +25,7 @@ object Dependencies {
     val catsCore = "2.4.2"
     val catsEffect = "2.4.0"
     val circe = "0.13.0"
-    val fs2 = "2.5.3"
+    val fs2 = "3.0.0"
     val log4j = "2.14.1"
     val mutationTestingElements = "1.7.0"
     val mutationTestingMetrics = "1.7.0"
