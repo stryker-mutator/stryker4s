5f6587fa6b04e2f0d55360f9fad7e197f2fac928
Hugo van Rijswijk <dev@hugovr.nl>
2021-04-08 16:36:38 +0200

    Update sttp to 3.3.0-RC1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -31,7 +31,7 @@ object Dependencies {
     val mutationTestingMetrics = "1.7.0"
     val pureconfig = "0.14.1"
     val scalameta = "4.4.11"
-    val sttp = "3.2.3"
+    val sttp = "3.3.0-RC1"
     val testInterface = "1.0"
     val weaponRegeX = "0.4.1"
   }
