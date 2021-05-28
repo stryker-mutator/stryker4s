6829d50591ef165c89a867c94286806e3d0198f0
Scala Steward <me@scala-steward.org>
2021-05-28 15:03:45 +0200

    Update scala-library to 2.12.14

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
   object versions {
     val scala211 = "2.11.12"
-    val scala212 = "2.12.13"
+    val scala212 = "2.12.14"
     val scala213 = "2.13.6"
     val scala3 = "3.0.0"
 
