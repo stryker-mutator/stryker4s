5ef57d36f8c679b40fac67a14e89a1a58f4b5899
Scala Steward <me@scala-steward.org>
2021-06-19 18:49:54 +0200

    Update circe, httpclient-backend-fs2 to 3.3.7

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -31,7 +31,7 @@ object Dependencies {
     val mutationTestingMetrics = "1.7.2"
     val pureconfig = "0.16.0"
     val scalameta = "4.4.21"
-    val sttp = "3.3.6"
+    val sttp = "3.3.7"
     val testInterface = "1.0"
     val weaponRegeX = "0.6.0"
   }
