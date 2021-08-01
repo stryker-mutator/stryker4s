b30bcc918777d03fab866213037d771a23f6bfb0
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-08-01 02:52:14 +0200

    Update cats-effect-testing-scalatest to 1.2.0 (#920)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -16,7 +16,7 @@ object Dependencies {
     val fullCrossScalaVersions = crossScalaVersions ++ Seq(scala211, scala3)
 
     // Test dependencies
-    val catsEffectScalaTest = "1.1.1"
+    val catsEffectScalaTest = "1.2.0"
     val mockitoScala = "1.16.37"
     val scalatest = "3.2.9"
 
