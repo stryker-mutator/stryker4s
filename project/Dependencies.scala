e42d840db0761ab4c82f559f1f954a53019ce6ec
Scala Steward <me@scala-steward.org>
2021-10-18 15:17:52 +0200

    Update scala3-library to 3.1.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
   object versions {
     val scala212 = "2.12.15"
     val scala213 = "2.13.6"
-    val scala3 = "3.0.2"
+    val scala3 = "3.1.0"
 
     /** Cross-versions for main projects
       */
