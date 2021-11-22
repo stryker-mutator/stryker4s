f55b196506b9fb43b53bbdd8d0aa1e6de36ec1bd
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-11-22 02:39:00 +0100

    Update pureconfig, pureconfig-sttp to 0.17.1 (#1016)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -27,7 +27,7 @@ object Dependencies {
     val log4j = "2.14.1"
     val mutationTestingElements = "1.7.6"
     val mutationTestingMetrics = "1.7.6"
-    val pureconfig = "0.17.0"
+    val pureconfig = "0.17.1"
     val scalameta = "4.4.30"
     val sttp = "3.3.17"
     val testInterface = "1.0"
