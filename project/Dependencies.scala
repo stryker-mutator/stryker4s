bfae7c128768d605cc6b09cfe4848e933b604bea
Scala Steward <me@scala-steward.org>
2022-01-13 20:57:26 +0100

    Update scala-library to 2.13.8

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt.*
 object Dependencies {
   object versions {
     val scala212 = "2.12.15"
-    val scala213 = "2.13.7"
+    val scala213 = "2.13.8"
     val scala3 = "3.1.0"
 
     /** Cross-versions for main projects
