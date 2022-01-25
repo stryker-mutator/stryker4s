200b59bf16f67678f408a00330ffc42875b6c4de
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-01-25 12:21:27 +0100

    Update slf4j-simple to 1.7.35 (#1083)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -29,7 +29,7 @@ object Dependencies {
     val mutationTestingMetrics = "1.7.8"
     val pureconfig = "0.17.1"
     val scalameta = "4.4.33"
-    val slf4j = "1.7.33"
+    val slf4j = "1.7.35"
     val sttp = "3.4.0"
     val testInterface = "1.0"
     val weaponRegeX = "0.6.0"
