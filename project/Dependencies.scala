197aee26dcbb63e43e5354763144878439653d5b
Scala Steward <me@scala-steward.org>
2022-05-06 16:25:40 +0200

    Update circe to 3.6.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -30,7 +30,7 @@ object Dependencies {
     val pureconfig = "0.17.1"
     val scalameta = "4.5.6"
     val slf4j = "1.7.36"
-    val sttp = "3.5.2"
+    val sttp = "3.6.0"
     val testInterface = "1.0"
     val weaponRegeX = "0.6.0"
   }
