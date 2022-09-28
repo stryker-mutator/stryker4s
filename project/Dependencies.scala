f07ab4434831c3f175115173f08ee4cad036ca93
Hugo van Rijswijk <git@hugovr.nl>
2022-09-28 19:04:02 +0200

    Update Scala versions to 2.12.17 and 2.13.9

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,8 +2,8 @@ import sbt.*
 
 object Dependencies {
   object versions {
-    val scala212 = "2.12.16"
-    val scala213 = "2.13.8"
+    val scala212 = "2.12.17"
+    val scala213 = "2.13.9"
     val scala3 = "3.2.0"
 
     /** Cross-versions for main projects
