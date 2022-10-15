977e8461d388cbae1f531ff0dd3a20a246cf1d2f
Scala Steward <scala_steward@virtuslab.com>
2022-10-15 11:32:10 +0000

    Update scalatest to 3.2.14

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -17,7 +17,7 @@ object Dependencies {
     // Test dependencies
     val catsEffectScalaTest = "1.4.0"
     val mockitoScala = "1.17.12"
-    val scalatest = "3.2.13"
+    val scalatest = "3.2.14"
 
     // Direct dependencies
     val catsCore = "2.8.0"
