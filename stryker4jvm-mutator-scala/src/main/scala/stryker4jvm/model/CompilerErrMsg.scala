c6204ae4016f4d0ecafb24cfd865f034ddefb4f3
Koen Reefman <k.h.a.reefman@student.utwente.nl>
2022-11-10 14:42:19 +0100

    Added missing file

---
+++ stryker4jvm-mutator-scala/src/main/scala/stryker4jvm/model/CompilerErrMsg.scala
@@ -0,0 +1,7 @@
+package stryker4jvm.model
+
+//This class is used to contain information about mutants that did not compile
+//It essentially exists so that we don't have to pass around the SBT specific compiler exception
+final case class CompilerErrMsg(msg: String, path: String, line: Integer) {
+  override def toString: String = s"$path:L$line: '$msg'"
+}
