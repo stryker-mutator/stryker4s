5439ec2f9e532616327d59d7b655f8e76032ad53
LvanArkel <lvanarkel@hotmail.nl>
2022-11-11 12:40:32 +0100

    Added LanguageMutator interface and relevant classes to jvm-core

--- stryker4jvm-core/src/main/scala/stryker4jvm/model/Mutant.scala
+++ stryker4jvm-core/src/main/scala/stryker4jvm/model/Mutant.scala
@@ -1,17 +1,12 @@
 package stryker4jvm.model
 
-import cats.Show
-import cats.syntax.show.*
 import mutationtesting.{Location, MutantResult, MutantStatus}
-import stryker4jvm.extensions.TreeExtensions.PositionExtension
-
-import scala.meta.{Position, Term}
 
 final case class MutantId(value: Int) extends AnyVal {
   override def toString(): String = value.toString
 }
 
-final case class MutantWithId(id: MutantId, mutatedCode: MutatedCode) {
+final case class MutantWithId[T](id: MutantId, mutatedCode: MutatedCode[T]) {
   def toMutantResult(status: MutantStatus, testsCompleted: Option[Int] = None, description: Option[String] = None) =
     MutantResult(
       id = id.toString(),
@@ -24,7 +19,7 @@ final case class MutantWithId(id: MutantId, mutatedCode: MutatedCode) {
     )
 }
 
-final case class MutatedCode(mutatedStatement: Term, metadata: MutantMetadata)
+final case class MutatedCode[T](mutatedStatement: T, metadata: MutantMetadata)
 
 /** Metadata of [[stryker4s.model.MutatedCode]]
   *
@@ -39,17 +34,6 @@ final case class MutatedCode(mutatedStatement: Term, metadata: MutantMetadata)
   */
 final case class MutantMetadata(original: String, replacement: String, mutatorName: String, location: Location) {
   def showLocation: String = {
-    import MutantMetadata.locationShow
-
-    location.show
-  }
-}
-
-object MutantMetadata {
-  def apply(original: String, replacement: String, mutatorName: String, position: Position): MutantMetadata =
-    MutantMetadata(original, replacement, mutatorName, position.toLocation)
-
-  implicit def locationShow: Show[Location] = Show.show(location =>
     s"${location.start.line}:${location.start.column} to ${location.end.line}:${location.end.column}"
-  )
+  }
 }
