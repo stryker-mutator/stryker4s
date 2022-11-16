5fcfd25da687468953cd7423c98132e44f1a471f
mcder <m.c.deroos@student.utwente.nl>
2022-11-16 19:16:21 +0100

    Completely converted stryker4jvm-core to pure java with fansi.Str being the exception See merge-notes-java-conversion.txt in root of module

--- stryker4jvm-core/src/main/java/stryker4jvm/core/reporting/Reporter.java
+++ stryker4jvm-core/src/main/java/stryker4jvm/core/reporting/Reporter.java
@@ -3,7 +3,7 @@ package stryker4jvm.core.reporting;
 import stryker4jvm.core.reporting.events.FinishedRunEvent;
 import stryker4jvm.core.reporting.events.MutantTestedEvent;
 
-public abstract class Reporter {
+public abstract class Reporter<C> {
     public abstract void mutantTested(MutantTestedEvent event);
-    public abstract void onRunFinished(FinishedRunEvent event);
+    public abstract void onRunFinished(FinishedRunEvent<C> event);
 }
