fc2ba2511caa563ffc03e8222a32ba9523ec2496
LvanArkel <lvanarkel@hotmail.nl>
2022-11-21 13:12:35 +0100

    ScalaFMT :(

--- stryker4jvm-mutator-scala/src/test/resources/scalaFiles/scala3File.scala
+++ stryker4jvm-mutator-scala/src/test/resources/scalaFiles/scala3File.scala
@@ -1,7 +1,7 @@
 // Tests support for new top-level and extension syntax
 
 object SeqOps {
-  extension [A, B](list: Seq[A]) {
+  extension[A, B](list: Seq[A]) {
     def myFoldRight(init: B)(f: (A, B) => B): B = list.foldRight(init)(f)
   }
 }
