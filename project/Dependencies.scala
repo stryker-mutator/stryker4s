385ea5242c7f479f22ccb5aeea3990501cbefa3a
Scala Steward <scala_steward@virtuslab.com>
2022-11-22 22:25:14 +0000

    Update fs2-core, fs2-io to 3.4.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -24,7 +24,7 @@ object Dependencies {
     val catsEffect = "3.4.1"
     val circe = "0.14.3"
     val fansi = "0.4.0"
-    val fs2 = "3.3.0"
+    val fs2 = "3.4.0"
     val mutationTestingElements = "1.7.11"
     val mutationTestingMetrics = "1.7.11"
     val pureconfig = "0.17.2"
