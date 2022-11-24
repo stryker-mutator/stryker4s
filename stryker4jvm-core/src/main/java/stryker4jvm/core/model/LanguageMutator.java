d8bff822cddb7ff166bf6880ee5a09a5f871ee73
LvanArkel <lvanarkel@hotmail.nl>
2022-11-24 11:12:25 +0100

    Ported LanguageMutator to Java

---
+++ stryker4jvm-core/src/main/java/stryker4jvm/core/model/LanguageMutator.java
@@ -0,0 +1,34 @@
+package stryker4jvm.core.model;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Map;
+
+class LanguageMutator<T extends AST> {
+    private final Parser<T> parser;
+    private final Collector<T> collector;
+    private final Instrumenter<T> instrumenter;
+
+    public LanguageMutator(Parser<T> parser, Collector<T> collector, Instrumenter<T> instrumenter) {
+        this.parser = parser;
+        this.collector = collector;
+        this.instrumenter = instrumenter;
+    }
+
+    public T parse(Path p) throws IOException {
+        return parser.parse(p);
+    }
+
+    public CollectedMutants<T> collect(AST tree) {
+        return collector.collect((T) tree);
+    }
+
+    public T instrument(AST source, Map<AST, List<MutantWithId<AST>>> mutations) {
+        return instrumenter.instrument((T) source,
+            (Map<T, List<MutantWithId<T>>>) (Map<T, ?>) mutations
+        );
+    }
+
+
+}
