c8a3714472c8301727d4c6c14940d53e98c2348a
mcder <m.c.deroos@student.utwente.nl>
2022-12-01 14:13:32 +0100

    convert stryker4jvm to use java core

--- stryker4jvm/src/main/scala/stryker4jvm/exception/UnableToFixCompilerErrorsException.scala
+++ stryker4jvm/src/main/scala/stryker4jvm/exception/UnableToFixCompilerErrorsException.scala
@@ -2,9 +2,11 @@ package stryker4jvm.exception
 
 import cats.data.NonEmptyList
 import cats.syntax.foldable.*
+import stryker4jvm.core.exception.Stryker4jvmException
+import stryker4jvm.model.CompilerErrMsg
 
 final case class UnableToFixCompilerErrorsException(errs: NonEmptyList[CompilerErrMsg])
-    extends Stryker4sException(
+    extends Stryker4jvmException(
       "Unable to remove non-compiling mutants in the mutated files. As a work-around you can exclude them in the stryker.conf. Please report this issue at https://github.com/stryker-mutator/stryker4s/issues\n"
         + errs
           .map(err => s"${err.path}: '${err.msg}'")
