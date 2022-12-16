96cd2162e05b6dffa351c706ce05bfe50778b5e7
Koen-Reefman <k.h.a.reefman@student.utwente.nl>
2022-12-16 14:00:34 +0100

    New scala mutator stubs

---
+++ stryker4jvm-new-mutator-scala/src/main/scala/stryker4jvm/mutator/scala/ScalaCollector.scala
@@ -0,0 +1,11 @@
+package stryker4jvm.mutator.scala
+
+import stryker4jvm.core.model.Collector
+import stryker4jvm.core.model.CollectedMutants
+
+class ScalaCollector extends Collector[ScalaAST] {
+
+  override def collect(x$1: ScalaAST): CollectedMutants[ScalaAST] = ???
+
+  
+}
---
+++ stryker4jvm-new-mutator-scala/src/main/scala/stryker4jvm/mutator/scala/ScalaMutator.scala
@@ -0,0 +1,7 @@
+package stryker4jvm.mutator.scala
+
+import stryker4jvm.core.model.LanguageMutator
+
+class ScalaMutator extends LanguageMutator[ScalaAST](new ScalaParser(), new ScalaCollector(), new ScalaInstrumenter()) {
+  
+}
