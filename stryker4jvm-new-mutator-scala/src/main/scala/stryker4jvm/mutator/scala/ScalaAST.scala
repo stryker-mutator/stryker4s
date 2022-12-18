f090e0c84c3d3be1899f322d7e7a892e1e1ae74a
Koen-Reefman <k.h.a.reefman@student.utwente.nl>
2022-12-18 16:59:18 +0100

    Added back the files in .vscode Scala Parser

--- stryker4jvm-new-mutator-scala/src/main/scala/stryker4jvm/mutator/scala/ScalaAST.scala
+++ stryker4jvm-new-mutator-scala/src/main/scala/stryker4jvm/mutator/scala/ScalaAST.scala
@@ -1,8 +1,9 @@
 package stryker4jvm.mutator.scala
 
 import stryker4jvm.core.model.AST
+import scala.meta.Source
 
-class ScalaAST extends AST {
+class ScalaAST(val source: Source = null) extends AST {
   override def syntax(): String = ???
   override def hashCode(): Int = ???
   override def equals(obj: Any): Boolean = ???
--- stryker4jvm-new-mutator-scala/src/main/scala/stryker4jvm/mutator/scala/ScalaParser.scala
+++ stryker4jvm-new-mutator-scala/src/main/scala/stryker4jvm/mutator/scala/ScalaParser.scala
@@ -1,9 +1,18 @@
 package stryker4jvm.mutator.scala
 
+import scala.meta.{Parsed, Source}
+
+import scala.meta.parsers.XtensionParseInputLike
 import stryker4jvm.core.model.Parser
-import java.nio.file.Path
+import java.nio
 
 class ScalaParser extends Parser[ScalaAST] {
 
-  override def parse(x$1: Path): ScalaAST = ???
+  override def parse(file: nio.file.Path): ScalaAST = {
+    val parsed = file.parse[Source];
+    parsed match {
+      case _: Parsed.Error => throw new Exception("Cannot parse")
+      case s               => new ScalaAST(source = s.get)
+    }
+  }
 }
