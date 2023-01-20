142f86979dfb5c42000ed0b4a3295d968f9d3aa6
Scala Steward <scala_steward@virtuslab.com>
2023-01-20 00:01:03 +0000

    Update scalameta to 4.7.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -28,7 +28,7 @@ object Dependencies {
     val mutationTestingElements = "1.7.11"
     val mutationTestingMetrics = "1.7.11"
     val pureconfig = "0.17.2"
-    val scalameta = "4.6.0"
+    val scalameta = "4.7.2"
     val slf4j = "2.0.6"
     val sttp = "3.8.8"
     val testInterface = "1.0"
