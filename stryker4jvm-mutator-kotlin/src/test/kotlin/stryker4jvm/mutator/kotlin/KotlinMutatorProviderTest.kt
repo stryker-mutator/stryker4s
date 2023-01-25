76aa67184d15be85c0dcbe1a025e13210f178151
mcder <m.c.deroos@student.utwente.nl>
2023-01-25 13:08:04 +0100

    Tiny update to kotlin tests to increase some coverage

---
+++ stryker4jvm-mutator-kotlin/src/test/kotlin/stryker4jvm/mutator/kotlin/KotlinMutatorProviderTest.kt
@@ -0,0 +1,36 @@
+package stryker4jvm.mutator.kotlin
+
+import stryker4jvm.core.config.LanguageMutatorConfig
+import stryker4jvm.core.model.InstrumenterOptions
+import stryker4jvm.core.model.LanguageMutator
+import stryker4jvm.mutator.kotlin.mutators.BooleanLiteralMutator
+import stryker4jvm.mutator.kotlin.utility.PsiUtility
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class KotlinMutatorProviderTest {
+    private val provider = KotlinMutatorProvider()
+
+    @Test
+    fun testSimpleProvider() {
+        val config = LanguageMutatorConfig("Kotlin", HashSet())
+        val logger = NoopLogger()
+        val options = InstrumenterOptions.SysProp
+        val mutator = provider.provideMutator(config, logger, options)
+        testBooleanLiteral(mutator, true)
+    }
+
+    @Test
+    fun testConfiguredProvider() {
+        val config = LanguageMutatorConfig(null, setOf(BooleanLiteralMutator.name))
+        val logger = NoopLogger()
+        val options = InstrumenterOptions.EnvVar
+        val mutator = provider.provideMutator(config, logger, options)
+        testBooleanLiteral(mutator, false)
+    }
+
+    private fun testBooleanLiteral(mutator: LanguageMutator<*>, expect: Boolean) {
+        val booleanLiteral = KotlinAST(PsiUtility.createPsiElement("false"))
+        assertEquals(expect, mutator.collect(booleanLiteral).mutations.isNotEmpty())
+    }
+}
---
+++ stryker4jvm-mutator-kotlin/src/test/kotlin/stryker4jvm/mutator/kotlin/NoopLogger.kt
@@ -0,0 +1,18 @@
+package stryker4jvm.mutator.kotlin
+
+import stryker4jvm.core.logging.LogLevel
+
+class NoopLogger : stryker4jvm.core.logging.Logger() {
+    override fun log(p0: LogLevel?, p1: String?) {
+
+    }
+
+    override fun log(p0: LogLevel?, p1: String?, p2: Throwable?) {
+
+    }
+
+    override fun log(p0: LogLevel?, p1: Throwable?) {
+
+    }
+
+}
