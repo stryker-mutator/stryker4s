ac84d9b226e0f33b9d48a65a3db5f06a710a0f1c
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2023-03-23 08:17:06 +0100

    Update mockito-scala-cats, ... to 1.17.14 (#1322)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -16,7 +16,7 @@ object Dependencies {
 
     // Test dependencies
     val catsEffectScalaTest = "1.5.0"
-    val mockitoScala = "1.17.12"
+    val mockitoScala = "1.17.14"
     val scalatest = "3.2.15"
 
     // Direct dependencies
