081a8e246fdb764f52de25b8e30c7fc874806d11
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2023-08-12 02:41:14 +0200

    Update circe, fs2 to 3.9.0 (#1397)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -30,7 +30,7 @@ object Dependencies {
     val pureconfig = "0.17.4"
     val scalameta = "4.8.7"
     val slf4j = "2.0.7"
-    val sttp = "3.8.16"
+    val sttp = "3.9.0"
     val testInterface = "1.0"
     val weaponRegeX = "1.1.1"
   }
