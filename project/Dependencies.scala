353995ddd68686460ac0bb7db78b2d6017811619
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2023-09-11 01:38:01 +0200

    Update scalameta to 4.8.10 (#1409)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -28,7 +28,7 @@ object Dependencies {
     val mutationTestingElements = "2.0.3"
     val mutationTestingMetrics = "2.0.3"
     val pureconfig = "0.17.4"
-    val scalameta = "4.8.8"
+    val scalameta = "4.8.10"
     val slf4j = "2.0.7"
     val sttp = "3.9.0"
     val testInterface = "1.0"
