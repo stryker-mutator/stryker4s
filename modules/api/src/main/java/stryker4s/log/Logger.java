e8fd49a9c1e666d7233c5eb723a3bed4022f29d7
Hugo van Rijswijk <git@hugovr.nl>
2023-11-16 15:25:41 +0100

    replace usage of var

--- modules/api/src/main/java/stryker4s/log/Logger.java
+++ modules/api/src/main/java/stryker4s/log/Logger.java
@@ -1,6 +1,7 @@
 package stryker4s.log;
 
 import java.util.function.Supplier;
+import java.util.Map;
 
 public interface Logger {
 
@@ -8,18 +9,20 @@ public interface Logger {
 
 	public void log(Level level, Supplier<String> msg, Throwable t);
 
-  /** 
+	/**
 	 * Whether colors are enabled in the log
-    */
+	 */
 	public default boolean colorEnabled() {
-		var env = System.getenv();
-    // Explicitly disable color https://no-color.org/
-    boolean notNoColor = !env.containsKey("NO_COLOR");
-    // If there is a TERM on Linux (or Windows Git Bash), assume we support color
-    boolean unixEnabled = env.containsKey("TERM");
-    // On Windows there's no easy way. But if we're in Windows Terminal or ConEmu, we can assume we support color
-    boolean windowsEnabled = env.containsKey("WT_SESSION") || (System.getenv().containsKey("ConEmuANSI") && System.getenv("ConEmuANSI").equals("ON"));
+		Map<String, String> env = System.getenv();
+		// Explicitly disable color https://no-color.org/
+		boolean notNoColor = !env.containsKey("NO_COLOR");
+		// If there is a TERM on Linux (or Windows Git Bash), assume we support color
+		boolean unixEnabled = env.containsKey("TERM");
+		// On Windows there's no easy way. But if we're in Windows Terminal or ConEmu,
+		// we can assume we support color
+		boolean windowsEnabled = env.containsKey("WT_SESSION")
+				|| (System.getenv().containsKey("ConEmuANSI") && System.getenv("ConEmuANSI").equals("ON"));
 
-    return notNoColor && (unixEnabled || windowsEnabled);
+		return notNoColor && (unixEnabled || windowsEnabled);
 	}
 }
