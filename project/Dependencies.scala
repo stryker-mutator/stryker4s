9885986045492f448b3ba148cada46f90174e44b
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2024-01-17 10:08:27 +0100

    build(deps): update cats-effect from 3.5.2 to 3.5.3 (#1494)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -20,7 +20,7 @@ object Dependencies {
 
     // Direct dependencies
     val catsCore = "2.10.0"
-    val catsEffect = "3.5.2"
+    val catsEffect = "3.5.3"
     val circe = "0.14.6"
     val fansi = "0.4.0"
     val fs2 = "3.9.3"
