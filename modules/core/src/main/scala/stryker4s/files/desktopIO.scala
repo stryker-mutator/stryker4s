0b1db172593c4148074587e6abfd7d0f3e821adf
Hugo van Rijswijk <git@hugovr.nl>
2024-12-18 13:27:14 +0100

    fix: disable fatal-warnings scalacOption when mutating (#1680)

--- modules/core/src/main/scala/stryker4s/files/desktopIO.scala
+++ modules/core/src/main/scala/stryker4s/files/desktopIO.scala
@@ -1,26 +1,23 @@
 package stryker4s.files
 
 import cats.effect.IO
+import cats.syntax.parallel.*
+import fs2.io.file.{Files, Path}
+
 import java.awt.Desktop
 import java.io.File
-import fs2.io.file.Files
-import fs2.io.file.Path
 
 trait DesktopIO {
   def attemptOpen(path: Path): IO[Unit]
 }
 
 class DesktopFileIO extends DesktopIO {
-  override def attemptOpen(path: Path): IO[Unit] =
-    isDesktopSupported.ifM(
-      Files[IO]
-        .exists(path)
-        .ifM(
-          openFile(path.toNioPath.toFile()),
-          IO.unit
-        ),
-      IO.unit
-    )
+  override def attemptOpen(path: Path): IO[Unit] = {
+    (isDesktopSupported, Files[IO].exists(path)).parTupled
+      .flatMap { case (desktopSupported, fileExists) =>
+        IO.whenA(desktopSupported && fileExists)(openFile(path.toNioPath.toFile()))
+      }
+  }
 
   def openFile(file: File): IO[Unit] = IO(Desktop.getDesktop.open(file))
 
