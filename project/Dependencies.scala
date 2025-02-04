7cf04cc5ca70d511b1d5c9df32b61a804fd38f27
Scala Steward <scala_steward@virtuslab.com>
2025-02-04 07:56:48 +0000

    build(deps): update mutation-testing-metrics-circe from 3.4.1 to 3.5.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -32,7 +32,7 @@ object Dependencies {
 
     val mutationTestingElements = "3.4.0"
 
-    val mutationTestingMetrics = "3.4.1"
+    val mutationTestingMetrics = "3.5.0"
 
     val scalameta = "4.12.7"
 
