8a6aa0a4d825f0c0d3d16abca74b9a5ccd183739
Hugo van Rijswijk <git@hugovr.nl>
2025-03-10 15:22:07 +0100

    chore: upgrade sttp-client to 4 (#1718)

--- modules/core/src/main/scala/stryker4s/report/DashboardReporter.scala
+++ modules/core/src/main/scala/stryker4s/report/DashboardReporter.scala
@@ -5,20 +5,20 @@ import cats.effect.{IO, Resource}
 import cats.syntax.foldable.*
 import fansi.Color.Red
 import fansi.{Bold, Str}
-import io.circe.Error
 import mutationtesting.{MetricsResult, MutationTestResult}
 import stryker4s.config.codec.CirceConfigEncoder
 import stryker4s.config.{Config, Full, MutationScoreOnly}
 import stryker4s.log.Logger
 import stryker4s.report.dashboard.DashboardConfigProvider
 import stryker4s.report.model.*
-import sttp.client3.*
-import sttp.client3.circe.{asJson, circeBodySerializer}
+import sttp.client4.*
+import sttp.client4.ResponseException.{DeserializationException, UnexpectedStatusCode}
+import sttp.client4.circe.*
 import sttp.model.{MediaType, StatusCode}
 
 class DashboardReporter(dashboardConfigProvider: DashboardConfigProvider[IO])(implicit
     log: Logger,
-    httpBackend: Resource[IO, SttpBackend[IO, Any]]
+    httpBackend: Resource[IO, Backend[IO]]
 ) extends Reporter
     with CirceConfigEncoder {
 
@@ -49,26 +49,26 @@ class DashboardReporter(dashboardConfigProvider: DashboardConfigProvider[IO])(im
       case Full =>
         import mutationtesting.circe.*
         request
-          .body(report)
+          .body(asJson(report))
       case MutationScoreOnly =>
         implicit val encoder: Encoder[ScoreOnlyReport] = Encoder.forProduct1("mutationScore")(r => r.mutationScore)
         request
-          .body(ScoreOnlyReport(metrics.mutationScore))
+          .body(asJson(ScoreOnlyReport(metrics.mutationScore)))
     }
   }
 
-  private def logResponse(response: Response[Either[ResponseException[String, Error], DashboardPutResult]]): Unit =
+  private def logResponse(response: Response[Either[ResponseException[String], DashboardPutResult]]): Unit =
     response.body match {
-      case Left(HttpError(errorBody, StatusCode.Unauthorized)) =>
+      case Left(UnexpectedStatusCode(errorBody, meta)) if meta.code == StatusCode.Unauthorized =>
         log.error(
           s"Error HTTP PUT '$errorBody'. Status code ${Red("401 Unauthorized")}. Did you provide the correct api key in the '${Bold
               .On("STRYKER_DASHBOARD_API_KEY")}' environment variable?"
         )
-      case Left(HttpError(errorBody, statusCode)) =>
+      case Left(UnexpectedStatusCode(errorBody, statusCode)) =>
         log.error(
           s"Failed to PUT report to dashboard. Response status code: ${Red(statusCode.code.toString())}. Response body: '$errorBody'"
         )
-      case Left(DeserializationException(original, error)) =>
+      case Left(DeserializationException(original, error, _)) =>
         log.warn(
           s"Dashboard report was sent successfully, but could not decode the response: '$original'. Error:",
           error
