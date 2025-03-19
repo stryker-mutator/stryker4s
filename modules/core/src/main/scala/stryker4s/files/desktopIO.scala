5be339d50104f41f76dfe0227e2278d7f20c094a
Hugo van Rijswijk <git@hugovr.nl>
2025-03-19 16:53:58 +0100

    chore: use fs2 process builders

--- modules/core/src/main/scala/stryker4s/files/desktopIO.scala
+++ modules/core/src/main/scala/stryker4s/files/desktopIO.scala
@@ -3,10 +3,11 @@ package stryker4s.files
 import cats.effect.IO
 import cats.syntax.parallel.*
 import fs2.io.file.{Files, Path}
+import fs2.io.process.{Process, ProcessBuilder}
+import fs2.text.utf8
 
 import java.awt.Desktop
 import java.io.File
-import scala.sys.process.*
 
 trait DesktopIO {
   def attemptOpen(path: Path): IO[Unit]
@@ -22,13 +23,18 @@ class DesktopFileIO extends DesktopIO {
 
   def openFile(file: File): IO[Unit] =
     IO(sys.props.get("os.version").exists(_.contains("WSL"))).flatMap { isWsl =>
-      if (isWsl)
+      if (isWsl) for {
         // Convert to windows-style path with wslpath utility and open with powershell
-        IO.blocking(s"wslpath -ma $file".!!.trim())
-          .flatMap(file => IO.blocking(s"powershell.exe -NoProfile -Command Start-Process file://$file".!!))
-          .void
+        file <- spawnProcess("wslpath", "-ma", file.toString()).use(processStdout)
+        _ <- spawnProcess("powershell.exe", "-NoProfile", "-Command", "Start-Process", s"file://$file")
+          .use(_.exitValue)
+      } yield ()
       else IO.blocking(Desktop.getDesktop.open(file))
     }
+
+  private def spawnProcess(command: String, args: String*) = ProcessBuilder(command, args.toList).spawn[IO]
+
+  private def processStdout(p: Process[IO]): IO[String] = p.stdout.through(utf8.decode).compile.string.map(_.trim())
 
   def isDesktopSupported: IO[Boolean] = IO(
     Desktop.isDesktopSupported && Desktop.getDesktop.isSupported(Desktop.Action.OPEN)
--- modules/core/src/main/scala/stryker4s/run/process/ProcessResource.scala
+++ modules/core/src/main/scala/stryker4s/run/process/ProcessResource.scala
@@ -1,12 +1,15 @@
 package stryker4s.run.process
 
-import cats.effect.{IO, Resource}
-
-import scala.sys.process.{ProcessBuilder, ProcessLogger}
+import cats.effect.IO
+import cats.syntax.flatMap.*
+import fs2.Stream
+import fs2.io.process.ProcessBuilder
+import fs2.text.{lines, utf8}
 
 object ProcessResource {
-  def fromProcessBuilder(pb: => ProcessBuilder)(logger: String => Unit) = {
-    val processLogger = ProcessLogger(logger(_))
-    Resource.make(IO(pb.run(processLogger)))(p => IO(p.destroy()))
+  def fromProcessBuilder(pb: ProcessBuilder, logger: String => IO[Unit]) = {
+    def log(s: Stream[IO, Byte]) = s.through(utf8.decode).through(lines).evalMap(logger)
+
+    pb.spawn[IO].flatTap(p => log(p.stdout).concurrently(log(p.stderr)).compile.drain.background)
   }
 }
