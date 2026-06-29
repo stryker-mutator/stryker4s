abcbbc8ecebb00ffe23f12b512a87cebdc280717
Hugo van Rijswijk <git@hugovr.nl>
2026-06-29 13:09:47 +0200

    chore: minor cats refactoring combinators

--- modules/core/src/main/scala/stryker4s/extension/DurationExtensions.scala
+++ modules/core/src/main/scala/stryker4s/extension/DurationExtensions.scala
@@ -1,7 +1,8 @@
 package stryker4s.extension
 
 import cats.data.Chain
-import cats.syntax.foldable.*
+import cats.data.Chain.*
+import cats.syntax.all.*
 
 import java.util.concurrent.TimeUnit
 import scala.concurrent.duration.Duration
@@ -9,28 +10,29 @@ import scala.concurrent.duration.Duration
 object DurationExtensions {
   implicit final class HumanReadableExtension(val duration: Duration) extends AnyVal {
     final def toHumanReadable: String = {
-      val units = Seq(TimeUnit.DAYS, TimeUnit.HOURS, TimeUnit.MINUTES, TimeUnit.SECONDS, TimeUnit.MILLISECONDS)
+      val units = Chain(TimeUnit.DAYS, TimeUnit.HOURS, TimeUnit.MINUTES, TimeUnit.SECONDS, TimeUnit.MILLISECONDS)
 
+      // Walk the units largest-first, emitting a part per non-zero unit.
       val timeStrings = units
-        .foldLeft((Chain.empty[String], duration.toMillis)) { case ((humanReadable, rest), unit) =>
+        .mapAccumulate(duration.toMillis) { (rest, unit) =>
           val name = unit.toString().toLowerCase()
           val result = unit.convert(rest, TimeUnit.MILLISECONDS)
           val diff = rest - TimeUnit.MILLISECONDS.convert(result, unit)
           val str = result match {
-            case 0    => humanReadable
-            case 1    => humanReadable :+ s"1 ${name.init}" // Drop last 's'
-            case more => humanReadable :+ s"$more $name"
+            case 0    => none
+            case 1    => s"1 ${name.init}".some // Drop last 's'
+            case more => s"$more $name".some
           }
-          (str, diff)
+          (diff, str)
         }
-        ._1
+        ._2
+        .flattenOption
 
-      timeStrings match {
-        case Chain()  => "0 seconds"
-        case Chain(a) => a
-        case _        =>
-          val (strings, last) = timeStrings.initLast.get
-          strings.mkString_(", ") + " and " + last
+      // `Chain`'s extractors aren't exhaustivity-aware, but these arms cover empty/one/many.
+      (timeStrings: @unchecked) match {
+        case Chain()       => "0 seconds"
+        case Chain(a)      => a
+        case init :== last => init.mkString_(", ") + " and " + last
       }
     }
   }
