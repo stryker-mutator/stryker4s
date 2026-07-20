c61e3361b1d4375d477301b2dd5d67e11a0d3a61
Hugo van Rijswijk <git@hugovr.nl>
2026-07-20 11:08:30 +0200

    fix(sbt2): detach state when running tasks to avoid errors bubbling up

--- modules/sbt/src/main/scala-3/stryker4s/sbt/PluginCompat.scala
+++ modules/sbt/src/main/scala-3/stryker4s/sbt/PluginCompat.scala
@@ -13,9 +13,12 @@ private[stryker4s] object PluginCompat {
     cp.map(a => conv.toPath(a.data))
 
   def runTask[T](taskKey: TaskKey[T], state: State): Option[Either[Incomplete, T]] =
-    val extracted = Project.extract(state)
-    val config = EvaluateTask.extractedTaskConfig(extracted, extracted.structure, state)
-    EvaluateTask(extracted.structure, taskKey.scopedKey, state, extracted.currentRef, config).map(_._2.toEither)
+    // Detach to avoid errors bubbling up
+    val detachedState = state.copy(currentCommand = state.currentCommand.map(_.withSource(None)))
+    val extracted = Project.extract(detachedState)
+    val config = EvaluateTask.extractedTaskConfig(extracted, extracted.structure, detachedState)
+    EvaluateTask(extracted.structure, taskKey.scopedKey, detachedState, extracted.currentRef, config)
+      .map(_._2.toEither)
 
   def mapValues[K, V, W](map: mutable.Map[K, V])(f: V => W): Map[K, W] =
     map.view.mapValues(f).toMap
